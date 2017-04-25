ec3541a5f819cb6c3a5cd3b468fc1b548c7ac681
Charles Gray <charles.gray@fungible.com>
2017-04-25 15:41:50 -0700

    Add jsonutil (builds on Linux)

---
+++ jsonutil/build-afl.mk
@@ -0,0 +1,22 @@
+ifeq ($(OS),Linux)
+
+# clear these for fuzzing
+CFLAGS :=
+LDFLAGS_ASAN :=
+
+# build with AFL
+# CC := ../../afl-2.41b/afl-clang
+CC := ../../afl-2.41b/afl-gcc
+
+FUZZ := ../../afl-2.41b/afl-fuzz
+
+FUNOS := ../../FunOS
+SRC := jsonutil.c fun_json.c fun_map.c fun_heap.c fun_set.c
+
+fuzztext: jsonutil
+	$(FUZZ) -i testcase_dir -o findings_dir -- ./jsonutil --in @@
+
+fuzzbinary: jsonutil
+	$(FUZZ) -i testcase_bindir -o findings_bindir -- ./jsonutil --inb @@
+
+endif
