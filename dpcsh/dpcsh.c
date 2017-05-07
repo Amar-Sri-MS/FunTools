2912bb0c099016888f3d92755170c2575bb8fad4
Bertrand Serlet <bertrand.serlet@fungible.com>
2017-05-06 18:59:48 -0700

    Making ^P ^N up-arrow and down-arrow work in dpcsh.

--- dpcsh/dpcsh.c
+++ dpcsh/dpcsh.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <termios.h>            //termios, TCSANOW, ECHO, ICANON
 
 #include <funos/fun_json.h>
 #include <funos/fun_commander.h>
@@ -38,27 +39,115 @@ static int _open_sock(const char *name) {
 	return sock;
 }
 
-static int getline_with_history(char **line, OUT size_t *len) {
-#if 0
-    if (!len) {
-	len = 100;
-	*line = realloc(*line, len);
+static void ensure_buffer_big_enough(OUT char **line, OUT size_t *capa, size_t len) {
+    if (len + 1 >= *capa) {
+        *capa = (*capa * 2) + 1;
+	*line = realloc(*line, *capa);
     }
-    char ch = getchar();
-    if (ch < 'a' || ch > 'z') {
-	printf("got char %d '%c'\n", ch, ch);
+}
+static void push_char(char **line, OUT size_t *capa, OUT size_t *len, char ch) {
+    ensure_buffer_big_enough(line, capa, (*len)+1);
+    (*line)[(*len)++] = ch;
+}
+static void push_string(char **line, OUT size_t *capa, OUT size_t *len, char *str) {
+    size_t l = strlen(str);
+    ensure_buffer_big_enough(line, capa, (*len)+l);
+    strcpy((*line) + (*len), str);
+    *len += l;
+}
+
+static char **history = NULL; // most recent first
+static int history_count = 0;
+
+static void append_to_history(char *line) {
+    size_t l = strlen(line);
+    if (!l) return;
+    if (line[l-1] == '\n') l--; // exclude '\n'
+    if (!l) return;
+    history = realloc(history, (history_count+1) * sizeof(char *));
+    if (history_count != 0) {
+        char *last = history[history_count-1];
+	if (!strncmp(last, line, l) && !last[l]) return; // same - don't add
     }
-#endif
-    return getline(line, len, stdin);
+    history[history_count++] = strndup(line, l);
+    // printf("History : %d\n", history_count);
+}
+static void use_history(char **line, OUT size_t *capa, OUT size_t *len, int history_index) {
+    if (!history) return; // no effect
+    if (history_index >= history_count) return;
+    char *str = history[history_index];
+    // erase current characters
+    for (int i = 0; i < (*len); i++) printf("%c[D", 27);
+    // kill rest of line
+    printf("%c[K", 27);
+    printf("%s", str);
+//		fflush(stdout);
+    *len = 0;
+    push_string(line, capa, len, str);
+}
+static void history_previous(char **line, OUT size_t *capa, OUT size_t *len, OUT int *history_index) {
+    if ((* history_index) > 0) (*history_index)--;
+    use_history(line, capa, len, *history_index);
+}
+static void history_next(char **line, OUT size_t *capa, OUT size_t *len, OUT int *history_index) {
+    if ((*history_index) + 1 < history_count) (*history_index )++;
+    use_history(line, capa, len, *history_index);
+}
+static int getline_with_history(OUT char **line, OUT size_t *capa) {
+    size_t len = 0;
+    int current_history = history_count;
+    ensure_buffer_big_enough(line, capa, 16);
+    while (true) {
+        char ch = getchar();
+	if (ch == 14 /* ^N */) {
+	    history_next(line, capa, &len, &current_history);
+	} else if (ch == 16 /* ^P */) {
+	    history_previous(line, capa, &len, &current_history);
+        } else if ((ch == EOF) || (ch == 4 /* ^D*/)) {
+	    (*line)[len] = 0;
+   	    append_to_history(*line);
+	    return len;
+	} else if (ch == 27) {
+	    if (getchar() != 91) continue;
+	    char ch2 = getchar();
+	    if (ch2 == 'A') {
+    		history_previous(line, capa, &len, &current_history);
+	    } else if (ch2 == 'B') {
+    		history_next(line, capa, &len, &current_history);
+	    } else {
+		// printf("Ignoring ch2=%d\n", ch2);
+	    }
+	} else if (ch == '\n') {
+	    write(STDOUT_FILENO, &ch, 1);
+	    push_char(line, capa, &len, ch);
+	    (*line)[len] = 0;
+   	    append_to_history(*line);
+	    return len;
+	} else if (ch == 127 /*DEL*/) {
+            if (len > 0) {
+	        printf("%c[D", 27);
+	        printf("%c[K", 27);
+		len--;
+	    }
+	} else {
+	// printf("GOT ch=%d\n", ch);
+	    write(STDOUT_FILENO, &ch, 1);
+	    push_char(line, capa, &len, ch);
+	}
+    }
 }
 
 static void _do_interactive(int sock) {
     char *line = NULL;
-    size_t len = 0;
+    size_t capa = 0;
     ssize_t read;
     int64_t tid = 1;
-
-    while ((read = getline_with_history(&line, &len)) > 0) {
+    static struct termios tios;
+    tcgetattr(STDIN_FILENO, &tios);
+    tios.c_lflag &= ~(ICANON | ECHO);          
+    tcsetattr(STDIN_FILENO, TCSANOW, &tios);
+    while ((read = getline_with_history(&line, &capa)) > 0) {
+	if ((read == 1) && (line[0] == '\n')) continue; // skip blank lines
         struct fun_json *json = fun_commander_line_to_command(line, &tid);
         if (!json) {
             printf("could not parse\n");
