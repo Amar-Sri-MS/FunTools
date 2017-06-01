e8d1c2b6342ce14df2e3035798c174e27ba2396d
Bertrand Serlet <bertrand.serlet@fungible.com>
2017-06-01 08:22:46 -0700

    Fixing a last minute change that broke my script.

--- dpcsh/test.py
+++ dpcsh/test.py
@@ -9,14 +9,15 @@ import thread
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
 def read_and_print():
-	buffer = sock.recv(4096)
+	chunk = 1024
+	buffer = sock.recv(chunk)
 	buffering = True
 	while buffering:
 		if "\n" in buffer:
 			(line, buffer) = buffer.split("\n", 1)
 			print line
 		else:
-			more = sock.recv(4096)
+			more = sock.recv(chunk)
 			if not more:
 				buffering = False
 			else:
@@ -26,7 +27,8 @@ def read_and_print():
 
 # Connect the socket to the port where the server is listening
 server_address = '/tmp/funos-dpc-text.sock'
-print 'connecting to %s' % server_address
+print "connecting to '%s'" % server_address
+
 try:
     sock.connect(server_address)
 except socket.error, msg:
@@ -35,10 +37,12 @@ except socket.error, msg:
 
 thread.start_new_thread(read_and_print, ())
 
-sock.sendall('echo "hello DPC!\n')
+sock.sendall('echo "hello DPC!"\n')
 sock.sendall("help\n")
 sock.sendall("fibo 10\n")
 sock.sendall("setenv $foo {x: y}\n")
 sock.sendall("getenv $foo\n")
 sock.sendall("garbage\n")
-sock.sendall('echo "all done!\n')
+sock.sendall('echo "all done!"\n')
+
+time.sleep(2) # important because otherwise the print thread dies too early
