6d0063f1c9dd3e2c9dd7f724962c6b7fd742ba67
Bertrand Serlet <bertrand.serlet@fungible.com>
2017-06-01 01:44:39 -0700

    Added a new option to dpcsh: --text_proxy

--- dpcsh/dpcsh.c
+++ dpcsh/dpcsh.c
@@ -10,7 +10,9 @@
 #include <termios.h>            //termios, TCSANOW, ECHO, ICANON
 #include <sys/types.h>
 #include <signal.h>            //termios, TCSANOW, ECHO, ICANON
+#include <pthread.h>
 
+#include <funos/fun_utils.h>
 #include <funos/fun_json.h>
 #include <funos/fun_commander.h>
 
@@ -139,6 +141,23 @@ static int getline_with_history(OUT char **line, OUT size_t *capa) {
     }
 }
 
+#define BLACK	"0;30"
+#define RED	"0;31"
+#define GREEN	"0;32"
+#define BLUE	"0;34"
+#define PURPLE	"0;35"
+
+#define LIGHT_GREEN	"1;32"
+#define LIGHT_BLUE	"1;34"
+#define LIGHT_PURPLE	"1;35"
+
+#define PRELUDE		"\e["
+#define POSTLUDE	"m"
+
+#define INPUT_COLORIZE	PRELUDE RED POSTLUDE
+#define OUTPUT_COLORIZE	PRELUDE BLUE POSTLUDE
+#define NORMAL_COLORIZE	PRELUDE BLACK POSTLUDE
+
 static void _do_process_cmd(int sock, char *line, ssize_t read, uint64_t *tid)
 {
 
@@ -150,7 +169,7 @@ static void _do_process_cmd(int sock, char *line, ssize_t read, uint64_t *tid)
             printf("could not parse: %s\n", error);
             return;
         }
-        fun_json_printf("input => %s\n", json);
+        fun_json_printf(INPUT_COLORIZE "input => %s" NORMAL_COLORIZE "\n", json);
         bool ok = fun_json_write_to_fd(json, sock);
 	if (!ok) {
 	    // try to reopen pipe
@@ -174,7 +193,7 @@ static void _do_process_cmd(int sock, char *line, ssize_t read, uint64_t *tid)
             printf("invalid json returned\n");
             return;
         }
-        fun_json_printf("output => %s\n", output);
+        fun_json_printf(OUTPUT_COLORIZE "output => %s" NORMAL_COLORIZE "\n", output);
         fun_json_release(output);
 }
 
@@ -248,79 +267,230 @@ int json_handle_req(int jsock, const char *path, char *buf, int *size) {
 	return r;
 }
 
-#define PORTNO 9001
+static ssize_t read_more(int fd, INOUT char **buffer, INOUT size_t *len, INOUT size_t *capacity) {
+	size_t wanted = 1024;
+	while (*len + wanted + 1 > *capacity) {
+		*capacity += *capacity + 1;
+		*buffer = realloc(*buffer, *capacity);
+	}
+	ssize_t n = read(fd, *buffer + *len, wanted);
+	// printf("Read %ld chars\n", n);
+	if (n > 0) {
+		*len += n;
+		(*buffer)[*len] = 0;
+	}
+	return n;
+}
 
-#define nelm(n)	(sizeof(n)/sizeof(n[0]))
-char *help_str[] = {
-	"--help:	you know",
-	"--proxy:	webproxy",
-	"--nocli:	no cli mode, type cmd as arg"
-};
-
-void
-help(void)
-{
-	int i;
-	printf("Help:\n");
-	for (i = 0; i < nelm(help_str); i++) {
-		printf("\t%s\n", help_str[i]);
+static ssize_t read_at_least_one_line(int fd, INOUT char **buffer, INOUT size_t *len, INOUT size_t *capacity) {
+	ssize_t so_far = 0;
+	while (true) {
+		ssize_t n = read_more(fd, buffer, len, capacity);
+		if (n == -1) return n;
+		if (n == 0) return so_far;
+		so_far += n;
+		if (strchr(*buffer, '\n')) return so_far;
 	}
 }
 
-int
-main(int argc, char *argv[])
-{
-	int sock;
-	int proxy_mode = 0;
-	int interractive_mode = 1;
-	
-	if (argc >= 2) {
-	    if (strcmp(argv[1], "--proxy") == 0) {
-			proxy_mode = 1;
-		}
-		else if (strcmp(argv[1], "--help") == 0) {
-			help();
-			return 0;
-		}
-		else if (argc > 2 && strcmp(argv[1], "--nocli") == 0) {
-			interractive_mode = 0;
-		}
-	}
-	
-	printf("FunOS Dataplane Control Shell%s\n",
-	       proxy_mode ? ": proxy mode" : "");
+static inline void write_string(int client_sock, const char *str, bool write_zero) {
+	write(client_sock, str, strlen(str));
+	//	if (write_zero) write(client_sock, "", 1);
+}
 
-	/* open a socket to FunOS */
-	sock = _open_sock(SOCK_NAME);
-	if (sock <= 0) {
-	    printf("*** Can't open socket\n");
-	    exit(1);
+static bool push_to_dpc_get_reply_to_client(const char *line, INOUT uint64_t *tid, int dpc_sock,  int client_sock) {
+	// return true if we should continue to process lines
+	const char *error;
+	struct fun_json *json = fun_commander_line_to_command(line, tid, &error);
+	if (!json) {
+		char buf[1000];
+		snprintf(buf, sizeof(buf), "*** Could not parse: %s - line: %s\n", error, line);
+		write_string(client_sock, buf, true);
+		return true;
 	}
-	if (proxy_mode) {
-		run_webserver(sock, PORTNO);
-		return 0;
+	bool ok = fun_json_write_to_fd(json, dpc_sock);
+	fun_json_release(json);
+	if (!ok) {
+		printf("*** Connection to dpc server failed\n");
+		return false;
 	}
+	/* receive a reply */
+	struct fun_json *output = fun_json_read_from_fd(dpc_sock);
+	if (!output) {
+		write_string(client_sock, "*** invalid json returned\n", true);
+		return true;
+	}
+	if (output->type == fun_json_error_type) {
+		printf("Got error reply '%s'\n", output->error_message);
+		printf("*** Received error from dpc server\n");
+		write_string(client_sock, "*** ", false);
+		write_string(client_sock, output->error_message, false);
+		write_string(client_sock, "\n", true);
+		fun_json_release(output);
+		return true;
+	}
+	char *pp2 = fun_json_to_text(output);
+	fun_json_release(output);
+	if (!pp2) {
+		printf("*** No output from dpc server\n");
+		return true;
+	}
+	printf("Got reply '%s'\n", pp2);
+	write_string(client_sock, pp2, false);
+	write_string(client_sock, "\n", true);
+	free(pp2);
+	return true;
+}
 
-	if (interractive_mode) {
-		_do_interactive(sock);
-		return 0;
+static void *handle_text_thread(void *arg) {
+	int client_sock = (uintptr_t) arg;
+	printf("New client on socket %d\n", client_sock);
+	int dpc_sock = _open_sock(SOCK_NAME);
+	if (dpc_sock <= 0) {
+		printf("*** Can't open socket to dpc\n");
+		return NULL;
 	}
-
+	_setnosigpipe(dpc_sock);
 	uint64_t tid = 1;
-	int i, n;
-	ssize_t len;
-	char buf[512];
+	size_t capacity = 1000;
+	char *buffer = malloc(capacity);
+	size_t len = 0;
+	while (true) {
+		ssize_t n = read_at_least_one_line(client_sock, &buffer, &len, &capacity);
+		if (n == -1) {
+			printf("*** Socket in error\n");
+			break;
+		}
+		if (!n) {
+			if (!len) break;
+			printf("Received '%s' then EOF\n", buffer);
+			push_to_dpc_get_reply_to_client(buffer, &tid, dpc_sock, client_sock);
+			break;
+		}
+		char *line_break = strchr(buffer, '\n');
+		while (line_break) {
+			*line_break = 0;
+			printf("Received line '%s'\n", buffer);
+			bool ok = push_to_dpc_get_reply_to_client(buffer, &tid, dpc_sock, client_sock);
+			if (!ok) break;
+			size_t this_line_len = line_break - buffer + 1;
+			memmove(buffer, line_break+1, len - this_line_len + 1);
+			len -= this_line_len;
+			line_break = strchr(buffer, '\n');
+		}
+	}
+	free(buffer);
+	close(dpc_sock);
+	printf("Closed client %d\n", client_sock);
+	return NULL;
+}
 
-	n = 0;
-	for (i = 2; i < argc; i++) {
-		n += snprintf(buf + n, sizeof(buf) - n, "%s ", argv[i]); 
+static void run_text_proxy(const char *client_sock_name) {
+	printf("Publishing %s\n", client_sock_name);
+	int listen_sock;
+	struct sockaddr_un local = { 0 }, remote = { 0 };
+	socklen_t s;
+
+	/* create a server socket */
+	listen_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+	assert(listen_sock > 0);
+
+	local.sun_family = AF_UNIX;
+
+	snprintf(local.sun_path, sizeof(local.sun_path), "%s", client_sock_name);
+	unlink(local.sun_path);
+
+	if (bind(listen_sock, (struct sockaddr *)&local, sizeof(local)) == -1) {
+		perror("bind");
+		exit(1);
+	}
+
+	if (listen(listen_sock, 1) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	/* main server loop */
+	while(1) {
+		// printf("[dpcsock] Listening for connection\n");
+		s = sizeof(remote);
+		int rsock = accept(listen_sock,
+				   (struct sockaddr *)&remote, &s);
+		_setnosigpipe(rsock);
+
+		if (rsock == -1) {
+			perror("accept");
+		} else {
+			pthread_t _handle_thread;
+			int r = pthread_create(&_handle_thread, NULL,
+					   handle_text_thread, (void*)(uintptr_t)rsock);
+			if (r) {
+				perror("pthread_create");
+				exit(1);
+			}
+		}
+	}
+}
+
+#define PORTNO 9001
+
+#define HELP	\
+	"\t--help: 	you know\n"\
+	"\t--proxy:	webproxy, browse 'http://localhost:9001'\n"\
+	"\t--nocli:	no cli mode, type cmd as arg\n"\
+	"\t--text_proxy:	text JSON proxy, use port '/tmp/funos-dpc-text.sock'\n"
+
+static void _do_cli(int argc, char *argv[], int sock) {
+	uint64_t tid = 1;
+	char buf[512];
+	int n = 0;
+	for (int i = 2; i < argc; i++) {
+		n += snprintf(buf + n, sizeof(buf) - n, "%s ", argv[i]);
 		printf("buf=%s n=%d\n", buf, n);
 	}
 
-	len = strlen(buf);
+	size_t len = strlen(buf);
 	buf[--len] = 0;	// trim the last space
 	printf(">> single cmd [%s] len=%zd\n", buf, len);
 	_do_process_cmd(sock, buf, len, &tid);
+}
 
+int main(int argc, char *argv[]) {
+	bool proxy_mode = false;
+	bool text_proxy_mode = false;
+	bool interractive_mode = false;
+
+	if (argc < 2) {
+		interractive_mode = true;
+	} else if (strcmp(argv[1], "--proxy") == 0) {
+		proxy_mode = true;
+	} else if (strcmp(argv[1], "--text_proxy") == 0) {
+		text_proxy_mode = true;
+	} else if (strcmp(argv[1], "--help") == 0) {
+		printf("Help: \n" HELP "\n");
+		return 0;
+	} else if (argc > 2 && strcmp(argv[1], "--nocli") == 0) {
+		interractive_mode = false;
+	} else {
+		printf("*** Usage: \n" HELP "\n");
+		exit(2);
+	}
+	printf("FunOS Dataplane Control Shell%s\n", proxy_mode ? ": proxy mode" : "");
+
+	/* open a socket to FunOS */
+	int sock = _open_sock(SOCK_NAME);
+	if (sock <= 0) {
+		printf("*** Can't open socket\n");
+		exit(1);
+	}
+	if (proxy_mode) {
+		run_webserver(sock, PORTNO);
+	} else if (text_proxy_mode) {
+		run_text_proxy("/tmp/funos-dpc-text.sock");
+	} else if (interractive_mode) {
+		_do_interactive(sock);
+	} else {
+		_do_cli(argc, argv, sock);
+	}
 	return 0;
 }
