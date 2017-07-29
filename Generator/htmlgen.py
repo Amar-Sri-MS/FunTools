e4624566f475cb597a6503488866d639b2e23e13
Robert Bowdidge <robert.bowdidge@fungible.com>
2017-07-28 17:43:35 -0700

    Redo HTML to generate documentation for each command represented by a single enum.

--- Generator/htmlgen.py
+++ Generator/htmlgen.py
@@ -5,18 +5,48 @@
 
 import utils
 
+def ReplaceField(field, union_map):
+  """Returns the actual field to draw for a field and a union map.
+
+  Replaces fields with type of a union key in the union map with the
+  field in the union with the same type as the value in the union map.
+  Allows descriptions to focus only on a particular alternative.
+  """
+  for union in union_map.keys():
+    substruct = union_map[union]
+    if field.type.IsRecord():
+      if field.type.base_type.node == union:
+        for f in field.subfields:
+          if f.type.IsRecord():
+            if f.type.base_type.node == substruct:
+              return f
+  return field
+
+
+  for f in field.subfields:
+    if f.type == replacement_struct:
+      return f
+  print('ReplaceField: could not find replacement for %s in %s' % (
+      union_map[field.type], field))
+  return None
+  
+
 class HTMLGenerator:
 
-  def Rows(self, struct):
-    """Returns an array of arrays of (field name, field width)"""
-    # TODO(Bowdidge): Support unions.  Perhaps create alternative structure without union?
+  def Rows(self, struct, union_map):
+    """Returns an array of arrays of (field name, field width)
+
+    Each top-level array represents a flit and the fields in the flit.
+    """
     fields = []
     rows = []
     current_row = []
     next_color = 0
     bytes_remaining = 64
 
+    # Field wrapping to next row.
     lingering_field_name = None
+    # Number of bits in lingering field.
     lingering_field_width = 0
     
     fields = list(struct.fields)
@@ -30,6 +60,10 @@ class HTMLGenerator:
           lingering_field_width = lingering_field_width - bytes_remaining
       else:
         next_field = fields.pop(0)
+
+        if union_map:
+          next_field = ReplaceField(next_field, union_map)
+
         if next_field.type.IsRecord():
           struct = next_field.type.base_type.node
           fields = list(struct.fields) + fields
@@ -48,20 +82,22 @@ class HTMLGenerator:
     return rows        
 
   def DrawFields(self, flit, row):
+    """Draws a row representing a single flit."""
     out = ''
     out += '  <div class="bitRow">\n'
     out += '    <div class="rowTitle">Flit %d</div>\n' % flit
     for (name, width) in row:
       bar_width = (1000 * width / 64) - 2
-      out += '    <div class="bar field" style="width: %dpx">%s</div>\n' % ( bar_width, name)
+      out += '    <div class="bar field" style="width: %dpx">%s</div>\n' % ( 
+        bar_width, name)
     out += '  </div>\n'
     return out
 
-  def BitmapForStruct(self, struct):
+  def BitmapForStruct(self, struct, union_map):
     out = ''
     out += '<div class="bitmap">\n'
     out += '  <div class="bitRow">\n'
-    out += '    <div class="rowTitle">Bits</div>\n'
+    out += '    <div class="rowTitle"><b>Bits</b></div>\n'
     out += '    <div class="bar keybar" style="width: 123px">63-56</div>\n'
     out += '    <div class="bar keybar" style="width: 123px">55-48</div>\n'
     out += '    <div class="bar keybar" style="width: 123px">47-40</div>\n'
@@ -72,7 +108,7 @@ class HTMLGenerator:
     out += '    <div class="bar keybar" style="width: 123px">7-0</div>\n'
     out += '  </div>\n'
 
-    rows = self.Rows(struct)
+    rows = self.Rows(struct, union_map)
     flit = 0
     for row in rows:
       out += self.DrawFields(flit, row)
@@ -98,6 +134,7 @@ class HTMLGenerator:
         background: #eeeeee;
         /* TODO(bowdidge): Mess with padding to get left column of both tables same width. */
         width: 85px;
+
 }
 .structTable td {
         padding: 10px;
@@ -116,11 +153,12 @@ class HTMLGenerator:
 /* Title bar of bitmap showing bit ranges. */
 .keybar {
   background-color: #f0f0f0;
-  height: 30px;
+  height: 40px;
   border: 1px solid gray;
   border-bottom: 1px solid black;
   border-left: 1px  solid black;
   border-right: 1px solid black;
+  font-weight: bold;
 }
 
 /* Block representing a single non-nested field. */
@@ -158,12 +196,13 @@ class HTMLGenerator:
 /* Left caption for a row. */
 .rowtitle {
   width: 200px;
+  height: 40px;
   display: inline-block;
   float: left;
+  background-color: #eeeeee;
 }
 /* Entire bitmap. */
 .bitmap {
-  border: solid 2px black;
   margin-bottom: 10px;
 }
 
@@ -242,7 +281,7 @@ class HTMLGenerator:
     out += '</table>'
     return out
 
-  def VisitRecordField(self, field, struct, level):
+  def VisitRecordField(self, field, struct, level, union_map):
     """Draws a struct as rows in a containing structure.
     
     struct is the """
@@ -278,23 +317,42 @@ class HTMLGenerator:
       return out
 
     for f in field.subfields:
+      if union_map:
+        f = ReplaceField(f, union_map)
       if f.type.IsRecord():
-        out += self.VisitRecordField(f, f.type.base_type.node, level + 2)
+        out += self.VisitRecordField(f, f.type.base_type.node, level + 2,
+                                     union_map)
       else:
-        out += self.VisitField(f, level + 1)
+        out += self.VisitField(f, level + 1, union_map)
     return out
 
   def VisitStruct(self, struct):
+    out = ''
+    union_inside = struct.ContainsUnion()
+    if not union_inside:
+      return self.VisitStructInternal(struct, struct.name, {})
+
+    out += '<h1>Commands Related To %s</h1>\n' % struct.name
+    out += '<p>There are several commands here.</p>'
+    for f in union_inside.fields:
+      if f.type.IsRecord():
+        substruct = f.type.base_type.node
+        out += self.VisitStructInternal(struct,
+                                        substruct.name,
+                                        {union_inside: substruct})
+    return out
+
+  def VisitStructInternal(self, struct, name, union_map):
     """Generates HTML documentation for a specific structure."""
     out = ''
-    out += '<a name="%s"></a>' % struct.name
-    out += '<h3>struct %s:</h3>\n' % struct.name
+    out += '<a name="%s"></a>' % name
+    out += '<h3>struct %s:</h3>\n' % name
     if struct.key_comment:
       out += '<p>%s</p>\n' % struct.key_comment
     if struct.body_comment:
       out += '<p>%s</p>\n' % struct.body_comment
 
-    out += self.BitmapForStruct(struct)
+    out += self.BitmapForStruct(struct, union_map)
 
     out += '<table class="structTable">\n'
     out += '<tr>\n'
@@ -305,10 +363,13 @@ class HTMLGenerator:
     # TODO(bowdidge): Fields and nested structures or unions should be
     # displayed in order of index.
     for field in struct.fields:
+      if union_map:
+        field = ReplaceField(field, union_map)
       if field.type.IsRecord():
-        out += self.VisitRecordField(field, field.type.base_type.node, 0)
+        out += self.VisitRecordField(field, field.type.base_type.node, 0,
+                                     union_map)
       else:
-        out += self.VisitField(field, 0)
+        out += self.VisitField(field, 0, union_map)
 
     # Tail comment comes after everything else.
     if struct.tail_comment:
@@ -321,12 +382,12 @@ class HTMLGenerator:
 
     return out
 
-  def VisitField(self, field, level):
+  def VisitField(self, field, level, union_map):
     """Generates HTML documentation for a specific field."""
     if len(field.packed_fields) != 0:
       out = ''
       for packed_field in field.packed_fields:
-        out += self.VisitField(packed_field, level + 1)
+        out += self.VisitField(packed_field, level + 1, union_map)
       return out
 
     # Draw a solid line at start of each flit to visually separate flits.
