31709842179ae653c9a60f5138d9c4cd8edc9997
Robert Bowdidge <robert.bowdidge@fungible.com>
2017-08-03 13:31:09 -0700

    Improve naming for packed fields. Use a common prefix if available. Ignore reserved fields at the start and end of the list of packed fields when choosing a name based on fields.

--- Generator/generator.py
+++ Generator/generator.py
@@ -713,18 +713,63 @@ class Checker:
     pass
 
 
-def LastNonReservedName(field_list):
-  last_name = None
+def CommonPrefix(name_list):
+  """Returns the longest prefix (followed by underbar) of all names.
+  Returns None if no longest prefix.
+  """
+  if len(name_list) < 2:
+    return None
+
+  first_name = name_list[0]
+  if not '_' in first_name:
+    return None
+
+  prefix = name_list[0].split('_')[0]
+  for name in name_list[1:]:
+    if not name.startswith(prefix + '_'):
+      return None
+  return prefix
+
+def FirstNonReservedName(field_list):
+  """Returns name of first field that is not a reserved field.
+  Returns None if no such field exists.
+  """
   first_name = None
   for field in field_list:
     if not field.IsReserved():
+      return field.name
+  return None
+
+def LastNonReservedName(field_list):
+  """Returns name of last field that is not a reserved field.
+  Returns None if no such field exists.
+  """
+  last_name = None
+  for field in field_list:
+    if not field.IsReserved():
       last_name = field.name
-      if first_name is None:
-        first_name = field.name
-  if last_name is not None:
-    return last_name
-  if last_name is first_name:
-    return 'z'
+  return last_name
+
+def ChoosePackedFieldName(fields):
+  """Chooses the name for a packed field base on the fields in that field."""
+  not_reserved_names = [f.name for f in fields if not f.IsReserved()] 
+  common_prefix = CommonPrefix(not_reserved_names)
+
+  if common_prefix:
+    return common_prefix + '_pack'
+
+  first_name = FirstNonReservedName(fields)
+  last_name = LastNonReservedName(fields)
+  # No fields.
+  if first_name is None and last_name is None:
+    return "empty_pack"
+
+  # One field.
+  if first_name == last_name:
+    return first_name + '_pack'
+
+  return first_name + "_to_" + last_name
+
 
 class Packer:
   """ Searches all structures for bitfields that can be combined.
@@ -824,7 +869,7 @@ class Packer:
             packed_field_width,
             type.BitWidth()))
 
-      new_field_name = fields[0].name + "_to_" + LastNonReservedName(fields)
+      new_field_name = ChoosePackedFieldName(fields)
       new_field = Field(new_field_name, type, min_start_bit, packed_field_width)
       new_field.line_number = fields[0].line_number
 
