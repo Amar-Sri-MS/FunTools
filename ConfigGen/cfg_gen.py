5f942183bbcf1c142e0cf56ef209e823ac6ba815
mboksanyi <michael.boksanyi@fungible.com>
2017-08-10 11:38:46 -0700

    Clarify scripts
    - Document general usage
    - Try and isolate jsonutil
    - Add some function descriptions

--- ConfigGen/cfg_gen.py
+++ ConfigGen/cfg_gen.py
@@ -1,16 +1,70 @@
 #!/usr/bin/python
 
+# cfg_gen.py
+# The config generator is intended to simplify configuration file maintenance,
+# now that we are getting more files. The files are all stored in a flat
+# layout (configs/*.cfg) and are combined into one out/default.cfg. This is
+# subject to change as requirements for different configurations grow.
+#
+# The input files are allowed to make two side-steps from the standard JSON
+# specification:
+#	- Allow comments
+#	- Allow hex values
+#
+# Both are intended to improve readability of the files, and leverage our
+# own jsonutil (which has a lenient parser) to do the initial parsing.
+#
+# Created by Michael Boksanyi, August 10 2017
+# Copyright Fungible Inc. 2017
+
 import glob, os, sys
 
 import json
 
+##### jsonutil-related commands #####
+def jsonutil_path():
+	return os.path.join('..', 'jsonutil')
+
+
+def build_jsonutil():
+
+	startdir = os.getcwd()
+
+	os.chdir(jsonutil_path())
+
+	if os.path.isfile('jsonutil'):
+		os.chdir(startdir)
+		return True
+
+	rc = os.system('make clean; make')
+	if rc != 0:
+		os.chdir(startdir)
+		return False
+
+	os.chdir(startdir)
+
+	return True
+
+# Input: json with comments and hex values
+# Output: json
+def standardize_json(in_cfg, out_cfg):
+
+	jsonutil_tool = os.path.join(jsonutil_path(), 'jsonutil')
+
+	os.system('%s -i %s -o %s' % (jsonutil_tool, in_cfg, out_cfg));
+
+
+# Standardize and combine multiple configuration files
+# into one config that will be used by FunOS
+# TBD: handle cases where different files refer to
+# the same keys
 def generate_config():
 
 	full_cfg = {}
 
 	for cfg in glob.glob("configs/*.cfg"):
-		
-		os.system('../jsonutil/jsonutil -i %s -o %s.tmp' % (cfg, cfg));
+
+		standardize_json(cfg, cfg+'.tmp')
 
 		f = open("%s.tmp" % cfg, 'r')
 		cfg_j = json.load(f)
@@ -29,32 +83,8 @@ def generate_config():
 	# XXX next step: output bjson
 
 
-def build_jsonutil():
-
-	startdir = os.getcwd()
-
-	os.chdir(os.path.join('..', 'jsonutil'))
-
-	if os.path.isfile('jsonutil'):
-		os.chdir(startdir)
-		return True
-
-	rc = os.system('make clean; make')
-	if rc != 0:
-		os.chdir(startdir)
-		return False
-
-	os.chdir(startdir)
-
-	return True
-
-
-
 if __name__ == "__main__":
 
-	# We are using jsonutil because the parser is more lenient than
-	# a classical json parser, allowing us to write more readable
-	# configurations thanks to comments and hex values
 	rc = build_jsonutil()
 	if rc == False:
 		print 'Failed to build jsonutil'
