b66198ee379e446150957ffb452ff70961e4da88
fred-stanley <fred.stanley@fungible.com>
2018-01-11 14:06:18 -0800

    modified script to generate default.cfg files. later revisit the build based override in funos during runtime

--- ConfigGen/cfg_gen.py
+++ ConfigGen/cfg_gen.py
@@ -32,8 +32,8 @@ build_override_cfg = {}
 #final config has build_override_cfg and sku config
 final_cfg = {}
 
-input_base = "."
-output_base = "."
+input_base = ""
+output_base = ""
 jsonutil_base = ""
 
 header = """ 
@@ -182,16 +182,24 @@ def output_default_config(build):
 	global module_cfg
 	global final_cfg
 
-	filepath = output_base + "/out/" + build
+	filepath = output_base 
 	if not os.path.exists(filepath):
 		os.makedirs(filepath)
 	
-	filename = output_base + "/out/" + build + "/" + "default_" + build + ".cfg"
+	filename = output_base + "default_" + build + ".cfg"
 	print filename
 	fout = open(filename, 'w')
 
 	output_header(fout)
 	output_cfg(fout)
+
+	#TODO fred fix with build based runtime override
+	if build == "posix":
+		filename = output_base + "default.cfg"
+		print filename
+		fout = open(filename, 'w')
+		output_header(fout)
+		output_cfg(fout)
 	
 	fout.close()
 
@@ -259,6 +267,8 @@ def main():
 	if rc == False:
  		print 'Failed to generate malta config'
 		sys.exit(1)
+
+	
 
 if __name__ == "__main__":
 	main()
