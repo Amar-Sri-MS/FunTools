5eecdd06477d59ddc22a9c4aaa8961f9bbe26a95
Robert Bowdidge <robert.bowdidge@fungible.com>
2018-02-23 12:36:30 -0800

    Get swap unit tests working again - changes in Linux header required mocking some of the Linux headers in the test machinery.

---
+++ Generator/test_helper/linux/types.h
@@ -0,0 +1,15 @@
+/* Dummy file for testing swap. */
+
+#define __u8 uint8_t
+#define __u16 uint16_t
+#define __u32 uint32_t
+#define __u64 uint64_t
+
+#define __le16 __u16
+#define __le32 __u32
+#define __le64 __u64
+
+#define __be16 __u16
+#define __be32 __u32
+#define __be64 __u64
+
