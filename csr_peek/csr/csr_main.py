ad3754f837d127ced61ed8e6ff4cbf209dce2a5c
Hariharan Thantry <hari.thantry@fungible.com>
2018-06-12 11:05:14 -0700

    Simplify to prepare for using offline DB

--- csr_peek/csr/csr_main.py
+++ csr_peek/csr/csr_main.py
@@ -25,26 +25,15 @@ class Slurper(object):
     def __arg_process(self, cmd_parser, other_args):
         ml_dir = module_locator().module_path()
         cmd_parser.add_argument("-d", "--csr-defs", help="CSR definitions", required=True, type=str)
-        cmd_parser.add_argument("-g", "--gen-lib", help="Generate the CSR library", action='store_true')
-
-        cmd_parser.add_argument("-l", "--lib-dir", help="Output directory for generated lib & include",
-                required=False, default=self.cwd, type=str)
-
-        cmd_parser.add_argument("-i", "--inc-dir", help="Output directory for generated lib & include",
-                required=False, default=self.cwd, type=str)
-
-        cmd_parser.add_argument("-c", "--cfg-dir", help="Output directory for JSONs", required=False,
+        cmd_parser.add_argument("-g", "--gen-cc", help="Generate the CC file", action='store_true')
+        cmd_parser.add_argument("-c", "--cfg-dir", help="Output directory for JSON/DB", required=False,
                 default=self.cwd, type=str)
-
-        cmd_parser.add_argument("-t", "--temp-dir", required=False,
-                help="Output directory for generated temporaries",type=str)
-
-        cmd_parser.add_argument("-k", "--keep-temps", required=False,
-                help="Keep generated artifacts", action='store_true')
-
         def_filter = os.path.join(ml_dir, "template", "csr_filter.yaml")
         cmd_parser.add_argument("-f", "--filter-file", help="Filter YAML file to reduce the size of the executable",
                 default=None, required=False, type=str)
+
+
+        self.other_args['gen_cc_dir'] = os.path.join(self.cwd, "csr", "libcsr", "src")
 
         self.other_args['tmpl_file'] = os.path.join(ml_dir, "template", "csr_rt.j2")
         self.other_args['csr_defs']  = os.path.join(ml_dir, "template", "csr_defs.yaml")
@@ -108,79 +97,16 @@ class Slurper(object):
         #w = Walker(yml_stream)
         #print "{}".format(w)
 
-    def __get_files(self, dirname, ext):
-        f_lst = []
-        for m_file in os.listdir(dirname):
-            if m_file.endswith(ext):
-                f_lst.append(os.path.join(dirname, m_file))
-        return f_lst
-    def __create_dirs(self, args):
-        args.lib_dir = self.__update_loc(args.lib_dir)
-        args.inc_dir = self.__update_loc(args.inc_dir)
-        if not args.temp_dir:
-            args.temp_dir = "/tmp/csr.{}".format(os.getpid())
-        else:
-            args.temp_dir=self.__update_loc(args.temp_dir)
-        file_mkr = Filer()
-
-        file_mkr.mkdir(args.lib_dir)
-        file_mkr.mkdir(args.inc_dir)
-        file_mkr.mkdir(args.temp_dir)
-    def __compile(self, tmp_files):
-
-        base_dir = os.path.join(self.ml_dir, "libcsr")
-        src_dir = os.path.join(base_dir, "src")
-        src_files = self.__get_files(src_dir, ".cpp")
-        lst = [m_file for m_file in tmp_files if m_file.endswith(".cpp")]
-        src_files.extend(lst)
-
-        inc_dir = [os.path.join(base_dir, "include")]
-        tmp_dir = []
-        for m_file in tmp_files:
-            if not m_file.endswith(".h"):
-                continue
-            dir_name = os.path.dirname(m_file)
-            if dir_name not in tmp_dir:
-                tmp_dir.append(dir_name)
-        inc_dir.extend(tmp_dir)
-
-        m_cpp = Compiler()
-        m_cpp.clean_files(tmp_files)
-        m_cpp.build_lib("libcsr", "static",
-                src_files, inc_dir, self.args.lib_dir, self.args.inc_dir)
-
-    def __clean(self, tmp_files):
-        dirs = []
-        for m_file in tmp_files:
-            basedir = os.path.dirname(m_file)
-            if basedir == self.args.lib_dir or \
-                    basedir == self.args.inc_dir:
-                        continue
-            if os.path.exists(m_file):
-                print "Removing tmp: {}".format(m_file)
-                os.remove(m_file)
-            if not basedir in dirs:
-                dirs.append(basedir)
-        for dirname in dirs:
-            if os.listdir(dirname):
-                continue
-            print "Removing directory: {}".format(dirname)
-            os.rmdir(dirname)
-
-    def compile(self):
-
-        if not self.args.gen_lib:
-            print "Not generating CSR lib"
+    def gen_cc(self):
+        if not self.args.gen_cc:
+            print "Not generating CC file"
             return
-        self.__create_dirs(self.args)
+        if not self.args.filter_file:
+            print "\n\n\n!!!!  WARNING   !!! Entire CSR database being generated in-memory. May NOT COMPILE\n\n\n"
 
-        cc_file = os.path.join(self.args.temp_dir, 'csr_gen.cpp')
+        cc_file = os.path.join(self.other_args['gen_cc_dir'], 'csr_gen.cpp')
         tmpl = TmplMgr(self.other_args['tmpl_file'])
         tmpl.write_cfg(cc_file, self.csr_root)
-        self.__compile([cc_file])
-        if self.args.keep_temps:
-            return
-        self.__clean([cc_file])
 
     def __str__(self):
         r_str = "{}".format(self.schema)
