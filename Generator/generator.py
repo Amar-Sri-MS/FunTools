Amar-Sri-MS/FunTools

commit 384b91b429b132871e9fc3962097afe8a8644360

Check for errors on indent failure and fall back to non-indent path

Generator/generator.py
@@ -905,6 +905,12 @@ def ReformatCodeWithIndent(source):
                        bufsize=1)
   # indent requires line feed after last line.
   out = p.communicate(source + '\n')
+
+  # If there was an indent fail
+  if (p.returncode != 0):
+    print "%s returned error %d, ignoring output" % (indent_path, p.returncode)
+    return None
+
   return out[0]
 
 def ReformatCodeWithClangFormat(source):

commit 2528aab066085289164af1b663ecf9ca5da4e27e

generator.py: fix indent support
Commit 384b91b4 ('Check for errors on indent failure and fall back to
non-indent path') introduced a check for failing indent invocations.
However, that leaves the generated files unindented.

The indentation failure is usually caused by an incompatible set of
indent flags, which may vary depending on the version of indent tool
used. For example '-nfcb' is not supported in a typical linux distro
variant.

Run a check of available options and only select the supported ones
before running indent to produce output as close as possible to the
expected one.

Signed-off-by: Marcin Nowakowski <marcin.nowakowski@fungible.com>

Generator/generator.py
@@ -399,12 +399,19 @@ def ReformatCodeWithIndent(source):
   if not indent_path:
     return None
 
-  args = [indent_path, '-sob', '-nfc1', '-nfcb', '-nbad', '-bap',
+  args_wanted = ['-sob', '-nfc1', '-nfcb', '-nbad', '-bap',
           '-nbc', '-br', '-brs', '-c33', '-cd33', '-ncdb', '-ce', '-ci4',
           '-cli0', '-d0', '-i8', '-ip0', '-l79', '-lp', '-npcs', '-npsl',
           # Don't format comments, get rid of extra blank lines.
           # Don't add whitespace in the middle of declarations.
           '-nsc', '-sob', '-di0']
+
+  args = [indent_path]
+
+  with open(os.devnull, 'r+') as dummy:
+    for arg in args_wanted:
+      if not subprocess.call([indent_path, arg], stdin=dummy, stdout=dummy, stderr=dummy):
+        args.append(arg)
 
   p = subprocess.Popen(args,
                        stdout=subprocess.PIPE,