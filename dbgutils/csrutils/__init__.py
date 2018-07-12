8cffa9b9d8a7b6903b39302cdb389b25d1b7e0d1
nponugoti <nponugoti@fungible.com>
2018-07-11 20:37:50 -0700

    Make it a module

--- dbgutils/csrutils/__init__.py
+++ dbgutils/csrutils/__init__.py
@@ -1,3 +1,2 @@
 from i2cutils import *
 from csrutils import *
-
--- dbgutils/setup.py
+++ dbgutils/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'csrutils',
+    name = 'dbgutils',
     version = '0.1.0',
     author = 'Nag Ponugoti(nag.ponugoti@fungible.com)',
     description = 'csr debug utilities',
@@ -12,7 +12,6 @@ setup(
     include_package_data=True,
     install_requires = [
         'aardvark_py',
-#        'binascii',
         'argparse',
         'jsocket',
         'logging',
