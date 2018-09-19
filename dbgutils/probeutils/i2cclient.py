c61dc596720783c0d07193d771ee33964703c850
nponugoti <nag.ponugoti@fungible.com>
2018-09-19 14:11:08 -0700

    Change the tcp port number for proxy

--- dbgutils/probeutils/i2cclient.py
+++ dbgutils/probeutils/i2cclient.py
@@ -15,7 +15,7 @@ logger = logging.getLogger("i2cclient")
 logger.setLevel(logging.INFO)
 
 class constants(object):
-    SERVER_TCP_PORT = 55668 
+    SERVER_TCP_PORT = 44444 
 
 class I2C_Client(object):
     def __init__(self, mode):
