aeef665d3f6d0d303eec7ff8e3cb999e7ca81903
Robert Bowdidge <robert.bowdidge@fungible.com>
2018-11-14 15:09:47 -0800

    Get rid of tabs in source files.

--- TraceParser/tdec_tmpl.py
+++ TraceParser/tdec_tmpl.py
@@ -8,130 +8,130 @@ import os
 import sys
 
 def RenderTemplate(template_name, page_dict):
-	"""Renders a Jinja template into HTML.
+    """Renders a Jinja template into HTML.
 
-	template_name is the filename for the template.
-	page_dict is a dictionary containing values used during rendering.
-	"""
-	this_dir = os.path.dirname(os.path.abspath(__file__))
-	env = jinja2.Environment(loader=jinja2.FileSystemLoader(this_dir))
-	
-	template = env.get_template(template_name)
-	return template.render(page_dict, env=env)
+    template_name is the filename for the template.
+    page_dict is a dictionary containing values used during rendering.
+    """
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(this_dir))
+    
+    template = env.get_template(template_name)
+    return template.render(page_dict, env=env)
 
 # Functions that should be drawn collapsed, either because they generate
 # too big a call graph or are uninteresting during performance analysis.
 functions_to_collapse = ['syslog',
-			 'printf', 'snprintf', 'fprintf',
-			 'malloc', 'calloc', 'realloc', 'free',
-			 'fun_calloc_threaded', 'fun_realloc_threaded',
-			 'fun_malloc_threaded', 'fun_free']
+             'printf', 'snprintf', 'fprintf',
+             'malloc', 'calloc', 'realloc', 'free',
+             'fun_calloc_threaded', 'fun_realloc_threaded',
+             'fun_malloc_threaded', 'fun_free']
 
 def AddLevels(tree, height, show_node):
-	"""Preprocesses a call tree in JSON form.
+    """Preprocesses a call tree in JSON form.
 
-	Marks uninteresting functions as collapsed.
-	"""
-	
-	tree['level'] = height
-	# Collapse on a node prevents it from being displayed at all.
-	tree['collapse'] = not show_node
-	show_children = tree['name'] not in functions_to_collapse
-	# collapse_children  on a node changes the +/- on that line to
-	# hint that the line can be expanded.
-	tree['children_collapse'] = not show_children
-	
-	if 'calls' in tree:
-		# Validation
-		for c in tree['calls']:
-			AddLevels(c, height + 1, show_children)
+    Marks uninteresting functions as collapsed.
+    """
+    
+    tree['level'] = height
+    # Collapse on a node prevents it from being displayed at all.
+    tree['collapse'] = not show_node
+    show_children = tree['name'] not in functions_to_collapse
+    # collapse_children  on a node changes the +/- on that line to
+    # hint that the line can be expanded.
+    tree['children_collapse'] = not show_children
+    
+    if 'calls' in tree:
+        # Validation
+        for c in tree['calls']:
+            AddLevels(c, height + 1, show_children)
   
 def PrepareTrees(roots, nodes):
-	"""Preprocess trees to be rendered in HTML.
+    """Preprocess trees to be rendered in HTML.
 
-	Convert numbered references to children into pointers to the
-	actual objects.  We numbered the nodes to make them easier to
-	render and process, but need the recursive structures to draw.
+    Convert numbered references to children into pointers to the
+    actual objects.  We numbered the nodes to make them easier to
+    render and process, but need the recursive structures to draw.
 
-	Also add a level number on each node so we can indent nodes,
-	and collapse certain uninteresting nodes.
+    Also add a level number on each node so we can indent nodes,
+    and collapse certain uninteresting nodes.
 
-	roots: list of indices for the nodes that are roots to the call
-	trees.
-	nodes: list of call tree nodes, as dictionaries.  The "id" field
-	gives the number of the element, which is referenced by roots and
-	in the "calls" field.
-	"""
-	# Implement with a worklist so that deep trees don't crash Python.
-	nodes_by_id = {}
+    roots: list of indices for the nodes that are roots to the call
+    trees.
+    nodes: list of call tree nodes, as dictionaries.  The "id" field
+    gives the number of the element, which is referenced by roots and
+    in the "calls" field.
+    """
+    # Implement with a worklist so that deep trees don't crash Python.
+    nodes_by_id = {}
 
-	for node in nodes:
-		nodes_by_id[node["id"]] = node
+    for node in nodes:
+        nodes_by_id[node["id"]] = node
 
-	for node in nodes:
-		if "calls" in node:
-			node["calls"] = [nodes_by_id[n] for n in node["calls"]]
-	roots = [nodes_by_id[root_id] for root_id in roots]
+    for node in nodes:
+        if "calls" in node:
+            node["calls"] = [nodes_by_id[n] for n in node["calls"]]
+    roots = [nodes_by_id[root_id] for root_id in roots]
 
-	for root in roots:
-		AddLevels(root, 0, True)
+    for root in roots:
+        AddLevels(root, 0, True)
 
-	return roots
+    return roots
 
 if __name__ == "__main__":
-	parser = OptionParser()
+    parser = OptionParser()
 
-	parser.add_option("-t", "--trace", dest="trace_file",
-			  help="JSON trace data", metavar="FILE")
-	parser.add_option('-d', '--dir', dest='dest_dir',
-			  help='Destination for HTML files')
-	parser.add_option('-c', '--core', dest='core', default='',
-			  help='Core number.  Used for output filenames')
-	(options, args) = parser.parse_args()
+    parser.add_option("-t", "--trace", dest="trace_file",
+              help="JSON trace data", metavar="FILE")
+    parser.add_option('-d', '--dir', dest='dest_dir',
+              help='Destination for HTML files')
+    parser.add_option('-c', '--core', dest='core', default='',
+              help='Core number.  Used for output filenames')
+    (options, args) = parser.parse_args()
 
-	if options.trace_file is None:
-		print "Need to specify a fungible option trace file"
-		sys.exit(1)
+    if options.trace_file is None:
+        print "Need to specify a fungible option trace file"
+        sys.exit(1)
 
-	dest_dir = ''
-	if options.dest_dir != None:
-		dest_dir = os.path.abspath(options.dest_dir)
+    dest_dir = ''
+    if options.dest_dir != None:
+        dest_dir = os.path.abspath(options.dest_dir)
 
-	# suffix on each generated file.
-	core = 0
-	if options.core:
-		core = int(options.core)
+    # suffix on each generated file.
+    core = 0
+    if options.core:
+        core = int(options.core)
 
-	if not os.path.isfile(options.trace_file):
-		sys.stderr.write('No such file: %s\n' % options.trace_file)
-		sys.exit(1)
-	f = open(options.trace_file, 'r')
-	try:
-		trace_json = json.load(f)
-	except Exception as e:
-		sys.stderr.write('Problems loading JSON trace: %s\n' % e)
-		sys.exit(1)
-	f.close()
+    if not os.path.isfile(options.trace_file):
+        sys.stderr.write('No such file: %s\n' % options.trace_file)
+        sys.exit(1)
+    f = open(options.trace_file, 'r')
+    try:
+        trace_json = json.load(f)
+    except Exception as e:
+        sys.stderr.write('Problems loading JSON trace: %s\n' % e)
+        sys.exit(1)
+    f.close()
 
-	#roots = ParseTraceFile(trace_json)
+    #roots = ParseTraceFile(trace_json)
 
-	trace_json['root_nodes']= PrepareTrees(trace_json['roots'],
-					       trace_json['call_nodes'])
+    trace_json['root_nodes']= PrepareTrees(trace_json['roots'],
+                           trace_json['call_nodes'])
 
-	trace_json['function_names'] = sorted(trace_json['function_stats'])
-	# print RenderTemplate('fn_table.tmpl', trace_json)
+    trace_json['function_names'] = sorted(trace_json['function_stats'])
+    # print RenderTemplate('fn_table.tmpl', trace_json)
 
-	out = RenderTemplate('template/call_tree.html', trace_json)
-	f = open(os.path.join(dest_dir, 'c%d.html' % core), 'w')
-	f.write(out)
-	f.close()
+    out = RenderTemplate('template/call_tree.html', trace_json)
+    f = open(os.path.join(dest_dir, 'c%d.html' % core), 'w')
+    f.write(out)
+    f.close()
 
-	out = RenderTemplate('template/function_list.html', trace_json)
-	f = open(os.path.join(dest_dir, 'function_list_%d.html' % core),
-		 'w')
-	f.write(out)
-	f.close()
-	
-	sys.stderr.write('Generated call tree and function summary.\n')
-	sys.exit(1)
+    out = RenderTemplate('template/function_list.html', trace_json)
+    f = open(os.path.join(dest_dir, 'function_list_%d.html' % core),
+         'w')
+    f.write(out)
+    f.close()
+    
+    sys.stderr.write('Generated call tree and function summary.\n')
+    sys.exit(1)
 
--- TraceParser/tutils_sim.py
+++ TraceParser/tutils_sim.py
@@ -2,69 +2,69 @@
 # code stay stable
 
 def ParseLine(trace_line):
-	"""Returns dictionary with values from Palladium trace line.
+    """Returns dictionary with values from Palladium trace line.
 
-	This routine splits the line once, removing much duplicate work.
-	"""
-	if trace_line[0] not in ['I', 'G']:
-		return None
-	args = trace_line.split()
-	return {'type': args[0],
-		'vpid': long(args[1]),
-		'address': long(args[2], 16),
-		'arg': long(args[3], 16),
-		'cycle': int(args[5][6:]),
-		'time': int(args[6][5:])}
+    This routine splits the line once, removing much duplicate work.
+    """
+    if trace_line[0] not in ['I', 'G']:
+        return None
+    args = trace_line.split()
+    return {'type': args[0],
+        'vpid': long(args[1]),
+        'address': long(args[2], 16),
+        'arg': long(args[3], 16),
+        'cycle': int(args[5][6:]),
+        'time': int(args[6][5:])}
 
 def is_return(trace_line):
-	return trace_line.split()[2] == "31"
+    return trace_line.split()[2] == "31"
 
 def is_data(trace_line):
-	return trace_line.startswith('G')
+    return trace_line.startswith('G')
 
 def is_instruction(trace_line):
-	return trace_line.startswith('I')
+    return trace_line.startswith('I')
 
 def is_instruction_miss(trace_line):
-	return False
+    return False
 
 def is_loadstore_miss(trace_line):
-	return False
+    return False
 
 def get_vpid(trace_line):
-	return int(trace_line.split()[1])
+    return int(trace_line.split()[1])
 
 def get_return_addr(trace_line):
-	return trace_line.split()[3]
+    return trace_line.split()[3]
 
 def get_address(trace_line):
-	try:
-		return long(trace_line.split()[2], 16)
-	except:
-		# XXX
-		return 0
+    try:
+        return long(trace_line.split()[2], 16)
+    except:
+        # XXX
+        return 0
 
 def get_asm(trace_line):
-	try:
-		return int(trace_line.split()[3], 16)
-	except:
-		# XXX
-		return 0
+    try:
+        return int(trace_line.split()[3], 16)
+    except:
+        # XXX
+        return 0
 
 def get_ts(trace_line):
-	time_txt = trace_line.split()[6]
-	timev = time_txt.split('=')[1]
-	return long(timev)	
+    time_txt = trace_line.split()[6]
+    timev = time_txt.split('=')[1]
+    return long(timev)    
 
 def get_cycle(trace_line):
-	cycle_txt = trace_line.split()[5]
-	cycle = cycle_txt.split('=')[1]
-	return long(cycle)	
+    cycle_txt = trace_line.split()[5]
+    cycle = cycle_txt.split('=')[1]
+    return long(cycle)    
 
 
 # FIXME
 def get_ccount(trace_line):
-	return 1
+    return 1
 
 def get_num_pipelines():
-	return 1
+    return 1
