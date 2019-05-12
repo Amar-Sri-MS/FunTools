a5fc2d9b043b388db6a543374fe241950fcb0497
Jimmy Yeap <jimmy.yeap@fungible.com>
2019-05-12 15:35:00 -0700

    Fixes for dq.sh

--- mips_dqr/dq.sh
+++ mips_dqr/dq.sh
@@ -11,15 +11,15 @@
 #
 
 ARGS=$*
-
 DUMMY_FILE=$(mktemp)
+JAR_DIR=$(dirname $0)
 
 echo "Creating dummy file for stdin: $DUMMY_FILE"
 
 # Run the dequeuer while we redirect stdin to a tail of the dummy file to it
 # (this provides a fake stdin). This is run as a background process to simplify
 # monitoring from this script.
-tail -f $DUMMY_FILE | java -jar "csdqer.jar" $ARGS &
+tail -f $DUMMY_FILE | java -jar "$JAR_DIR/csdqer.jar" $ARGS &
 
 # Record the PID of the java program
 DQR_PID=$!
@@ -33,6 +33,7 @@ while true; do
         sleep 5
     else
         echo "EOF" > $DUMMY_FILE
+        rm $DUMMY_FILE
 
         # The PID return code will be reserved within the shell until
         # this is called, so there is no danger of PID reuse causing
