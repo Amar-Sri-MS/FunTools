7ac76f39ba9238f848e8e247aab7f4df69e4e0c3
Viraj Bhide <viraj.bhide@fungible.com>
2019-10-06 23:18:47 -0700

    Shutdown the run_sc container on COMe before triggering warm/cold reset of the system

--- COMe/ResetFs1600.sh
+++ COMe/ResetFs1600.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-NOBOOT="/tmp/NoCOMeReboot.sh"
+NOREBOOT="/tmp/NoCOMeReboot.sh"
+FUN_ROOT="/opt/fungible"
 
 if [[ "$EUID" -ne 0 ]]; then
 	printf "Please run as ROOT EUID=$EUID\n"
@@ -9,10 +10,20 @@ fi
 
 echo "Running $0"
 
-if [[ -f $NOBOOT ]]; then
+if [[ -f $NOREBOOT ]]; then
 	printf "Aborting reboot request"
-	printf "Please remove file $NOBOOT"
+	printf "Please remove file $NOREBOOT"
 	exit
+fi
+
+FAST_REBOOT=0
+if [[ $# -eq 1 ]]; then
+	ARG=$1
+	if [[ $ARG == "-f" ]] || [[ $ARG == "--fast" ]]; then
+		FAST_REBOOT=1
+	else
+		printf "Unsupported option %s\n" $ARG
+	fi
 fi
 
 SSHPASS=`which sshpass`
@@ -34,17 +45,26 @@ printf "Poll BMC:  %s\n" $BMC_IP
 
 BMC="-P password: -p superuser ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no sysadmin@$BMC_IP"
 
-printf "WARNING: Sending BMC request to reboot host\n"
+printf "Shutdown the storage agent\n"
+$FUN_ROOT/StorageController/etc/start_sc.sh stop
+
 printf "\n**********************************************\n"
 printf "\n!!! Hold on to the bars, we are going down !!!\n"
 printf "\n**********************************************\n"
 
-COUNT=0
-while [[ $COUNT -lt 120 ]]; do
-	REBOOT=$(sshpass $BMC "exec $REBOOT_FILE" > /dev/null 2>&1)
-        sleep 1
-        let COUNT=COUNT+1
-done
+if [[ $FAST_REBOOT -eq 1 ]]; then
+	printf "Triggering fast reboot\n"
+	/sbin/reboot
+else
+	printf "WARNING: Sending BMC request to reboot host\n"
+	COUNT=0
+	while [[ $COUNT -lt 120 ]]; do
+		REBOOT=$(sshpass $BMC "exec $REBOOT_FILE" > /dev/null 2>&1)
+		sleep 1
+		let COUNT=COUNT+1
+	done
 
-# Should never reach here
-printf "!!! COMe was not able to send reset request to BMC !!!"
+	# Should never reach here
+	printf "!!! COMe was not able to send reset request to BMC !!!"
+fi
+
