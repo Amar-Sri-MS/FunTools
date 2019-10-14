61124b8078a301e1e1af8185dd51c2819b0e75c1
Parag Manjrekar <pmanjrekar@vnc-shared-06.fungible.local>
2019-10-14 16:11:23 -0700

    Revert "paragm: local setup with private TCP port and very slow speed."
    This reverts commit 49d000e16d5848e29afd34dbbc5be4c4b8fe114b.

--- dbgutils/probeutils/i2cproxy.py
+++ dbgutils/probeutils/i2cproxy.py
@@ -22,7 +22,7 @@ logger = logging.getLogger("i2cproxy")
 logger.setLevel(logging.INFO)
 
 class constants(object):
-    SERVER_TCP_PORT = 55555
+    SERVER_TCP_PORT = 44444
 
 def catch_exception(f):
     @functools.wraps(f)
