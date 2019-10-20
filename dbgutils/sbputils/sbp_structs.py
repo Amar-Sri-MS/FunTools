ab6368736abe1c4a2af5a7dd2e172ee180c1f065
Parag Manjrekar <pmanjrekar@vnc-shared-04.fungible.local>
2019-10-19 20:04:19 -0700

    add definitions of 0xFE0B0000-Diagnostic/fetchOTP

--- dbgutils/sbputils/sbp_structs.py
+++ dbgutils/sbputils/sbp_structs.py
@@ -215,6 +215,7 @@ commandEnum = {
     0xFE020001 : 'Diagnostic/ReadPubKeyBoot/SM',
     0xFE030000 : 'Diagnostic/SetUpgradeFlag/CM',
     0xFE030001 : 'Diagnostic/SetUpgradeFlag/SM',
+    0xFE0B0000 : 'Diagnostic/fetchOTP',
 
     0xFF000000 : 'Initialization/InitOTP/CM',
     0xFF000001 : 'Initialization/InitOTP/SM',
