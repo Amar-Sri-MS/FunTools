27b4e5ded987503d695674c9f776abd75d29d9ad
Renat Idrisov <renat.idrisov@fungible.com>
2019-11-20 14:30:22 -0800

    Add timeouts and quotes

--- dpcfuzz/src/dpcclient.ts
+++ dpcfuzz/src/dpcclient.ts
@@ -5,16 +5,21 @@ export class DPCClient {
   private socket: Net.Socket;
   private buffer: string;
   private error?: (e: any) => void;
+  private timeout_function?: () => void;
+  private timeout?: ReturnType<typeof setTimeout>;
+  private timeout_ms: number;
 
   constructor(host?: string, port?: number) {
     const h = host ? host : "127.0.0.1";
     const p = port ? port : 40221;
     this.socket = new Net.Socket();
     this.socket.connect({ port: p, host: h }, () => {
-      console.log('DPC client connected\n');
+      process.stdout.write("DPC client connected\n");
     });
+    this.socket.on("close", () => {process.stdout.write("DPC client disconnected\n")});
     this.buffer = "";
     this.transactionId = 0;
+    this.timeout_ms = 0;
   }
 
   public onData(callback: (d: any, error: boolean) => void, once?: boolean) {
@@ -22,6 +27,10 @@ export class DPCClient {
       this.buffer += s;
       if (this.countBuf("{") > 0 && this.countBuf("[") === this.countBuf("]")
       && this.countBuf("{") === this.countBuf("}")) {
+        if (this.timeout) {
+          clearTimeout(this.timeout);
+          this.timeout = undefined;
+        }
         const response = JSON.parse(this.buffer.replace(/\n/gm, ""));
         if (!response && this.error) this.error("No result");
         if (!response["result"]) {
@@ -43,11 +52,38 @@ export class DPCClient {
     this.socket.on("error", callback);
   }
 
-  public submit(command: string, args: any[]): void {
-    this.socket.write(JSON.stringify({"verb": command, "tid": this.transactionId++, "arguments": args}) + "\n");
+  public onTimeout(ms: number, callback: () => void) {
+    this.timeout_ms = ms;
+    this.timeout_function = callback;
+    this.socket.setTimeout(ms, callback);
+  }
+
+  public submit(command: string, args: any[], raw?: boolean): void {
+    if (this.timeout_ms != 0 && this.timeout_function) {
+      this.timeout = setTimeout(this.timeout_function, this.timeout_ms);
+    }
+    const a : any[] = (raw && raw === true) ? args : args.map(this.quote, this);
+    this.socket.write(JSON.stringify({"verb": command, "tid": this.transactionId++, "arguments": a}) + "\n");
+  }
+
+  public end(): void {
+    this.socket.end();
   }
 
   private countBuf(char: string): number {
     return this.buffer.split(char).length - 1;
+  }
+
+  private quote(a: any): any {
+    if (Array.isArray(a)) {
+      return ["quote", a.map(this.quote, this)];
+    }
+    if (a instanceof Object) {
+      const result: any = {}
+      for (const k in a) {
+        result[k] = this.quote(a);
+      }
+    }
+    return a;
   }
 }
