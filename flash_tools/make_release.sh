ed4149d60ae87a03154f127b344e49d9560f0f44
Fabrice Ferino <fabrice.ferino@fungible.com>
2020-01-16 12:25:37 -0800

    make_release can now take as argument a bundle number

--- flash_tools/make_release.sh
+++ flash_tools/make_release.sh
@@ -1,34 +1,60 @@
 #!/bin/bash -e
 
-function show_help()
+show_help ()
 {
     echo "make_release.sh: Prepare files for creating a release or a start certificate"
     echo ""
     echo "Usage:"
-    echo "./make_release.sh version"
+    echo "./make_release.sh [-b] <bundle or build number>"
     echo ""
+    echo "-h help: show this message"
+    echo "-b bundle: number is a bundle number"
+
 }
+
+build_number_of_bundle_number ()
+{
+    # retrieve the build properties file
+    wget -O - "http://dochub.fungible.local/doc/jenkins/master/fs1600/"$1"/bld_props.json" | \
+	python3 -c "import sys,json; print(json.load(sys.stdin)['components']['funsdk_flash_images'])"
+}
+
 
 if [[ `uname` != "Linux" ]] ; then
     echo "Sorry. This script can only be run on Linux"
     exit 1
 fi
 
-if [[ ( $@ == "--help") ||  $@ == "-h" ]] ; then
-    show_help
-    exit 0
-fi
+BUNDLE_FLAG=0
+while getopts "b" opt; do
+    case "$opt" in
+	h) show_help
+	   exit 0
+	   ;;
+	b) BUNDLE_FLAG=1
+	   ;;
+    esac
+done
 
-# JSON specification argument required
+shift $(($OPTIND - 1))
+
+# argument required
 if [  $# != 1 ] ; then
     show_help
-    echo "Missing argument: version"
+    echo "Missing argument: build number or bundle number"
     exit 1
 fi
 
-echo "SKUP Release"
+if [ $BUNDLE_FLAG -eq 1 ]; then
+    BUILD_NUMBER=$(build_number_of_bundle_number $1)
+else
+    BUILD_NUMBER=$1
+fi
+
+echo "SDKUP Release for build number "$BUILD_NUMBER
 mkdir -p ${WORKSPACE}/SDK_RELEASE
-${WORKSPACE}/FunSDK/scripts/bob --sdkup -s ${WORKSPACE}/SDK_RELEASE -C ${WORKSPACE}/FunSDK-cache --version=$1 release funos.posix-extra
+${WORKSPACE}/FunSDK/scripts/bob --sdkup -s ${WORKSPACE}/SDK_RELEASE -C ${WORKSPACE}/FunSDK-cache \
+	    --version=$BUILD_NUMBER release funos.posix-extra
 
 
 # if the current branch in FunTools is not master, then build flash_tools on top of the SDK
@@ -37,7 +63,7 @@ flash_tools_br=`cat ${WORKSPACE}/FunTools/.git/HEAD`
 if [[ ${flash_tools_br} !=  *refs/heads/master ]]; then
     ## for testing our changes in the FunTools/flash_tools area
     echo "WARNING: using the currently checked out branch of FunTools"
-    ${WORKSPACE}/FunSDK/scripts/bob --build -s ${WORKSPACE}/SDK_RELEASE --version=$1 flash_tools
+    ${WORKSPACE}/FunSDK/scripts/bob --build -s ${WORKSPACE}/SDK_RELEASE --version=$BUILD_NUMBER flash_tools
 fi
 
 echo "Injecting keys..."
