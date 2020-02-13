cae2a92cede2f44aaff828283630cc16cce53d6d
Charles Gray <charles.gray@fungible.com>
2020-02-12 17:26:33 -0800

    s/bundle configs/feature sets/

--- flash_tools/release.py
+++ flash_tools/release.py
@@ -87,7 +87,7 @@ def main():
                 "FunSDK/sbpfw/eeproms",
                 "FunSDK/sbpfw/firmware/chip_f1_emu_0",
                 "FunSDK/sbpfw/pufrom/chip_f1_emu_0",
-                "bundle_configs",
+                "feature_sets",
                 ]
         sdkpaths = [os.path.join(args.sdkdir, path) for path in paths]
 
