d0cad94c74a50611269695596c05c83791701061
Marcin Nowakowski <marcin.nowakowski@fungible.com>
2020-03-02 17:23:11 +0100

    flash_tools: change release tarball extension to tgz
    Signed-off-by: Marcin Nowakowski <marcin.nowakowski@fungible.com>

--- flash_tools/release.py
+++ flash_tools/release.py
@@ -231,7 +231,7 @@ def main():
         tarfiles.append('image.json')
         tarfiles.append('mmc_image.json')
 
-        with tarfile.open('sdk_signed_release.tar.gz', mode='w:gz') as tar:
+        with tarfile.open('sdk_signed_release.tgz', mode='w:gz') as tar:
             for f in tarfiles:
                 tar.add(f)
 
