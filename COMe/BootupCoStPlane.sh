ff582f5c7ee10f53a103687be558ff9d9ea63666
David Peet <davepeet@gmail.com>
2020-03-19 14:15:49 -0700

    SWSYS-542 Configure the management stack on the F1s

--- COMe/BootupCoStPlane.sh
+++ COMe/BootupCoStPlane.sh
@@ -112,6 +112,8 @@ DPCSH_TIMEOUT="10000"
 if [[ -f $FUN_ROOT/FunSDK/bin/Linux/dpcsh/dpcsh && -c $F1_0_NVME && -c $F1_1_NVME ]]; then
 	$FUN_ROOT/FunSDK/bin/Linux/dpcsh/dpcsh --pcie_nvme_sock=$F1_0_NVME --nvme_cmd_timeout=$DPCSH_TIMEOUT --tcp_proxy=$BMC_F1_0_DPC_PORT > $BMC_DPCSH_F1_0_LOGF 2>&1 &
 	$FUN_ROOT/FunSDK/bin/Linux/dpcsh/dpcsh --pcie_nvme_sock=$F1_1_NVME --nvme_cmd_timeout=$DPCSH_TIMEOUT --tcp_proxy=$BMC_F1_1_DPC_PORT > $BMC_DPCSH_F1_1_LOGF 2>&1 &
+	# Configure the management stack on the F1.
+	[ -f $FUN_ROOT/bin/come_config_mgmt.sh ] && $FUN_ROOT/bin/come_config_mgmt.sh
 fi
 
 echo "$0 DONE!!! (`date`)"
