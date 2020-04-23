e3f32c392c132fc008940521198967c0f662e6f4
Renat Idrisov <renat.idrisov@fungible.com>
2020-04-22 17:31:02 -0700

    Saving benchmarking scripts for future reference

---
+++ dpcsh/benchmark/echo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2.7
+import socket
+import json
+
+HOST = 'localhost'
+PORT = 44444
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by', addr
+while True:
+    f = conn.makefile()
+    data = f.readline()
+    if not data: break
+    j = json.loads(data)
+    conn.sendall(json.dumps({"result": "ok", "tid": j["tid"]}) + '\n')
+conn.close()
