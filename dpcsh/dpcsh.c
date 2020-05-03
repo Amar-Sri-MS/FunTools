2f4648fed7a70c91a20ce6ed94909b53a73ebccf
Bertrand Serlet <bertrand.serlet@fungible.com>
2020-05-02 17:50:53 -0700

    Increasing retries 15sec -> 30sec

--- dpcsh/dpcsh.c
+++ dpcsh/dpcsh.c
@@ -81,8 +81,8 @@ static bool _verbose_log = false;
 #define DEFAULT_NVME_CMD_TIMEOUT_MS "0"
 
 /* socket connect retry parameters */
-#define RETRY_DEFAULT (15)  /* retry for 15 seconds */
-#define RETRY_NOARG   (15)
+#define RETRY_DEFAULT (30)  /* retry for 30 seconds */
+#define RETRY_NOARG   (30)
 static uint16_t connect_retries = RETRY_DEFAULT;
 
 // We stash argv[0]
