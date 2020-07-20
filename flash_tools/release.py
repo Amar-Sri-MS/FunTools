02bf45698b659bcc679f1962f6241c3714e08f20
Marcin Nowakowski <marcin.nowakowski@fungible.com>
2020-07-20 12:51:36 +0200

    flash_tools/release: add nvdimm path
    Signed-off-by: Marcin Nowakowski <marcin.nowakowski@fungible.com>

--- flash_tools/release.py
+++ flash_tools/release.py
@@ -100,6 +100,7 @@ def main():
         paths = [ "bin",
                 "FunSDK/sbpfw/roms",
                 "FunSDK/sbpfw/eeproms",
+                "FunSDK/nvdimm_fw",
                 "feature_sets",
                 ]
         sdkpaths = [os.path.join(args.sdkdir, path) for path in paths]
