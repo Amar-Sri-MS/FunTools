9877fdc0f252a709f85cd2f199cb8997b272c724
David Daney <david.daney@fungible.com>
2020-07-23 19:08:37 -0700

    cfg_gen.py: Allow chip specific "module" overrides.
    If both:
      modules/abc/foo/default.cfg
      chip/modules/abc/foo/default.cfg
    
    exist, then override any settings in modules/abc/foo/default.cfg with
    those in chip/modules/abc/foo/default.cfg
    
    This allows generic modules specifications to be combined with chip
    specific values to generate a combined set of modules parameters for a
    given chip.

--- ConfigGen/cfg_gen.py
+++ ConfigGen/cfg_gen.py
@@ -205,6 +205,12 @@ def _generate_funos_default_config(config_root_dir, output_dir,
     funos_default_config = jsonutils.merge_dicts(funos_default_config, stats_cfg)
 
     modules_cfg = _generate_modules_config(config_root_dir)
+
+    config_chip_dir = os.path.join(config_root_dir, target_chip)
+    if os.path.exists(config_chip_dir):
+        chip_modules_cfg = _generate_modules_config(config_chip_dir)
+        modules_cfg = jsonutils.merge_dicts_recursive(modules_cfg, chip_modules_cfg)
+
     funos_default_config = jsonutils.merge_dicts(funos_default_config, modules_cfg)
 
     if not os.path.exists(output_dir):
