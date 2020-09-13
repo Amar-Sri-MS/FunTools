a84a571a95b1e7220bc56bb058dde1d604315d48
Charles Gray <charles.gray@fungible.com>
2020-09-12 19:27:57 -0700

    Add a missing file

---
+++ scripts/httpfile.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Random access read a file from http as a file-like using "range" headers
+"""
+
+import urllib
+
+## courtesy: https://stackoverflow.com/questions/7829311/is-there-a-library-for-retrieving-a-file-from-a-remote-zip/7852229#7852229
+
+class HttpFile(object):
+    def __init__(self, url):
+        self.url = url
+        self.offset = 0
+        self._size = -1
+
+    def size(self):
+        if self._size < 0:
+            f = urllib.request.urlopen(self.url)
+            self._size = int(f.headers["Content-length"])
+        return self._size
+
+    def read(self, count=-1):
+        req = urllib.request.Request(self.url)
+        if count < 0:
+            end = self.size() - 1
+        else:
+            end = self.offset + count - 1
+        req.headers['Range'] = "bytes=%s-%s" % (self.offset, end)
+        f = urllib.request.urlopen(req)
+        data = f.read()
+        # FIXME: should check that we got the range expected, etc.
+        chunk = len(data)
+        if count >= 0:
+            assert chunk == count
+        self.offset += chunk
+        return data
+
+    def seek(self, offset, whence=0):
+        if whence == 0:
+            self.offset = offset
+        elif whence == 1:
+            self.offset += offset
+        elif whence == 2:
+            self.offset = self.size() + offset
+        else:
+            raise Exception("Invalid whence")
+
+    def tell(self):
+        return self.offset
+
+    def closed(self):
+        return False
