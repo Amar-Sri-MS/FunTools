27e895fc778849dc3f83857bca287300b3fd19b8
Marcin Nowakowski <marcin.nowakowski@fungible.com>
2020-09-29 16:28:41 +0200

    release.py: add bundle generation [WIP]
    This is an initial commit that adds the framework for generating an
    installer bundle using release script. It does not generate a usable
    bundle yet.
    
    Signed-off-by: Marcin Nowakowski <marcin.nowakowski@fungible.com>

---
+++ flash_tools/install_tools/setup.sh
@@ -0,0 +1,118 @@
+#!/bin/bash -e
+
+setup_err() {
+	trap_code=$?
+	set +x
+	if [[ $trap_code -ne 0 ]]; then
+		log_msg "Failed in setup. Please contact Fungible, Inc. support"
+		[ -s stderr.txt ] && log_msg "STDERR is as follows:" && cat stderr.txt
+		echo "Upgrade error:" $trap_code > $LAST_ERROR
+		echo -n "Last progress indicator: " >> $LAST_ERROR
+		cat $PROGRESS >> $LAST_ERROR
+		echo "Last stderr output:" >> $LAST_ERROR
+		[ -s stderr.txt ] && cat stderr.txt >> $LAST_ERROR
+		rm -f $PROGRESS
+	else
+		log_msg "Install/Upgrade successful."
+		echo 100 > $PROGRESS
+	fi
+	sync
+	exit $trap_code
+}
+
+log_msg() {
+	echo "
+[$(date '+%D %T')] $PROG: $1";
+}
+
+
+# main
+PROG=$0
+PROGRESS=/tmp/cclinux_upgrade_progress # a reboot will clean up this tmpfs file.
+LAST_ERROR=/tmp/cclinux_upgrade_error # a reboot will clean up this tmpfs file.
+
+ccfg_install=''
+downgrade=''
+
+if [[ $# -ne 0 ]]
+then
+	while [[ $# -gt 0 ]]
+	do
+		case $1 in
+		install)
+			;;
+		install-downgrade)
+			downgrade='true'
+			;;
+		ccfg=*)
+			ccfg_install="ccfg-${1/ccfg=/}.signed.bin"
+			;;
+		*)
+			echo "
+	Usage: sudo ./${PROG}.sh
+		${PROG}.sh install [ccfg=config_name]
+		${PROG}.sh install-downgrade
+
+	Where,
+		Option install is to flash the DPU and install CCLinux software
+		Option install-downgrade should be used when installing an older bundle to attempt
+			DPU's firmware downgrade. This is not guaranteed to always work.
+		If ccfg is specified, a file called ccfg-<config_name>.signed.bin will be programmed as ccfg.
+	"
+			exit 1;
+			;;
+		esac
+		shift
+	done
+fi
+
+if [ -f $PROGRESS ]
+then
+	log_msg "Previous upgrade in progress... Aborting..."
+	log_msg "System should be rebooted to complete."
+	log_msg "You may recover by removing $PROGRESS."
+	exit 1
+fi
+
+# install the trap handler _after_ the initial in-progress check
+trap "setup_err" HUP INT QUIT TERM ABRT EXIT
+
+echo 1 > $PROGRESS
+
+[[ $(id -u) != 0 ]] && log_msg "You need to be root to run the installer" && exit 1
+
+log_msg "Installing ..."
+set -x
+
+echo 10 > $PROGRESS
+
+log_msg "Installing and configuring cclinux software and DPU firmware"
+pwd=$PWD
+
+echo 25 > $PROGRESS
+
+#funos_sdk_version=$(sed -ne 's/^funsdk=\(.*\)/\1/p' .version || echo latest)
+
+log_msg "Upgrading DPU firmware"
+log_msg "*** NOT IMPLEMENTED YET ***"
+
+#if [[ $downgrade ]]; then
+#	./opt/fungible/bin/run_fwupgrade.py --offline --offline-root $PWD/opt/fungible -U --ws ./opt/fungible/funos --version latest --force --downgrade
+#	./opt/fungible/bin/run_fwupgrade.py --offline --offline-root $PWD/opt/fungible -U --ws ./opt/fungible/funos --version latest --force --downgrade --active
+#else
+#	./opt/fungible/bin/run_fwupgrade.py --offline --offline-root $PWD/opt/fungible -U --ws ./opt/fungible/funos --version $funos_sdk_version
+#fi
+#if [[ $ccfg_install ]]; then
+#	./opt/fungible/bin/run_fwupgrade.py --offline --offline-root $PWD/opt/fungible --upgrade-file ccfg=$ccfg_install --ws ./opt/fungible/funos
+#	./opt/fungible/bin/run_fwupgrade.py --offline --offline-root $PWD/opt/fungible --upgrade-file ccfg=$ccfg_install --ws ./opt/fungible/funos --active
+#fi
+
+echo 50 > $PROGRESS
+
+log_msg "Upgrading CCLinux"
+log_msg "*** NOT IMPLEMENTED YET ***"
+
+echo 100 > $PROGRESS
+
+sync
+exit 0
