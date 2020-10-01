102f69e83889c144a407572840c655702e226223
David Daney <david.daney@fungible.com>
2020-10-01 14:31:51 -0700

    add_vmlinux_and_sign.sh: Remove obsolete script.

--- cclinux-tools/install.sh
+++ cclinux-tools/install.sh
@@ -13,7 +13,6 @@ target_dir=$SDK_INSTALL_DIR/bin/scripts
 
 mkdir -p $target_dir
 
-install -t $target_dir add_vmlinux_and_sign.sh
 install -t $target_dir strip_tree.sh
 install -t $target_dir deploy_factory.sh
 install -t $target_dir deploy_ro_root.sh
