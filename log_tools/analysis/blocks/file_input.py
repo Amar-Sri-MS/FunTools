527dcdece7e72537eadc8917241db07b812cf0fa
Sourabh Jain <sourabh.jain@fungible.com>
2020-10-02 16:44:20 +0530

    Added Multiline logs input parser

--- log_tools/analysis/blocks/file_input.py
+++ log_tools/analysis/blocks/file_input.py
@@ -13,6 +13,7 @@ import glob
 import gzip
 import io
 import os
+import re
 
 from blocks.block import Block
 
@@ -55,3 +56,59 @@ class TextFileInput(Block):
         logdir = self.env['logdir']
         return self.file_pattern.replace('${logdir}', logdir)
 
+
+class MultilineTextFileInput(TextFileInput):
+    """ Reads multiline input specified by a pattern from the text files """
+
+    def __init__(self):
+        super().__init__()
+
+    def set_config(self, cfg):
+        super().set_config(cfg)
+
+        self.pattern = cfg['pattern']
+
+    def process(self, iters):
+        pattern = self._replace_file_vars()
+
+        input_files = glob.glob(pattern)
+
+        # Assume that sorting files in timestamp order provides
+        # input in sorted order. We might enforce lexicographic order
+        # if it proves too difficult to enforce copying files with their
+        # original timestamp.
+        input_files.sort(key=os.path.getmtime)
+
+        for file in input_files:
+            if file.endswith('.gz'):
+                with gzip.open(file, mode='rt', encoding='ascii', errors='replace') as f:
+                    multiline_logs = []
+                    for line in f:
+                        # No need to parse if the log line is empty
+                        if not line: continue
+
+                        # Check if the current line is start of a new log and there are lines from
+                        # previous logs to parse
+                        if multiline_logs and self._check_for_match(line):
+                            yield (None, None, self.uid, None, ''.join(multiline_logs))
+                            multiline_logs = []
+                        multiline_logs.append(line)
+                    yield (None, None, self.uid, None, ''.join(multiline_logs))
+            else:
+                with open(file, 'r', encoding='ascii', errors='replace') as f:
+                    multiline_logs = []
+                    for line in f:
+                        # No need to parse if the log line is empty
+                        if not line: continue
+
+                        # Check if the current line is start of a new log and there are lines from
+                        # previous logs to parse
+                        if multiline_logs and self._check_for_match(line):
+                            yield (None, None, self.uid, None, ''.join(multiline_logs))
+                            multiline_logs = []
+                        multiline_logs.append(line)
+                    yield (None, None, self.uid, None, ''.join(multiline_logs))
+
+    def _check_for_match(self, line):
+        m = re.match(self.pattern, line)
+        return True if m else False
--- log_tools/analysis/pipeline.py
+++ log_tools/analysis/pipeline.py
@@ -59,6 +59,7 @@ class BlockFactory(object):
         better.
         """
         self.plugins['TextFileInput'] = file_input.TextFileInput
+        self.plugins['MultilineTextFileInput'] = file_input.MultilineTextFileInput
         self.plugins['FunOSInput'] = log_parsers.FunOSInput
         self.plugins['ISOFormatInput'] = log_parsers.ISOFormatInput
         self.plugins['MsecInput'] = log_parsers.MsecInput
