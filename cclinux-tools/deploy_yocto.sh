cbd0f6681f33dd26cf2e0a39e93043a9327513c2
David Daney <david.daney@fungible.com>
2020-11-03 12:17:37 -0800

    deploy_yocto.sh: Add /etc/modprobe.d/bonding.conf

--- cclinux-tools/deploy_yocto.sh
+++ cclinux-tools/deploy_yocto.sh
@@ -7,3 +7,9 @@ tar Jxf bin/cc-linux-yocto/mips64hv/fun-image-mips64r6hv-mips64r6.tar.xz -C $DEP
 #
 
 rm -f $DEPLOY_ROOT/etc/ntp.conf
+
+#
+# By default, don't create bondX devices when bonding is loaded
+#
+
+echo 'options bonding max_bonds=0' > $DEPLOY_ROOT/etc/modprobe.d/bonding.conf
