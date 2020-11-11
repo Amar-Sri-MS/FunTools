8f47a4695425e5018e141d342ea2c7ffd4e23efa
Sourabh Jain <sourabh.jain@fungible.com>
2020-11-11 14:07:57 +0530

    tuple_to_dict helper function in the Block class

--- log_tools/analysis/blocks/block.py
+++ log_tools/analysis/blocks/block.py
@@ -19,3 +19,15 @@ class Block(object):
     def set_config(self, config):
         """ Sets the config for this block """
         pass
+
+    def tuple_to_dict(self, tuple):
+        """
+        Convert the tuple that we use to communicate between blocks
+        into a dict
+        """
+        return {'datetime': tuple[0],
+                'usecs': tuple[1],
+                'uid': tuple[2],
+                'display_time': tuple[3],
+                'line': tuple[4]
+                }
