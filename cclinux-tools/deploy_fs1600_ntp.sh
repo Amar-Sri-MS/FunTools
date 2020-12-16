9e3d5f3f6b2be66b462fd038d3f4d9e74d539589
David Daney <david.daney@fungible.com>
2020-12-15 17:38:13 -0800

    deploy_fs1600_ntp.sh: Force NTP sync before continuing boot.

--- cclinux-tools/deploy_fs1600_ntp.sh
+++ cclinux-tools/deploy_fs1600_ntp.sh
@@ -7,3 +7,8 @@ chmod 0444 $DEPLOY_ROOT/etc/ntp.conf
 cp bin/mips64/Linux/vlan-bmc $DEPLOY_ROOT/etc/init.d
 chmod 0755 $DEPLOY_ROOT/etc/init.d/vlan-bmc
 ln -s ../init.d/vlan-bmc $DEPLOY_ROOT/etc/rc5.d/S00vlan-bmc
+
+#
+# Force NTP to synchronize before continuing the boot.
+#
+sed -i -E '/settick\(\)\{/a\\        $DAEMON -u ntp:ntp -p $PIDFILE -g -q' $DEPLOY_ROOT/etc/init.d/ntpd
