cb0c7ffe7a88a4eed364a74b065f988c08cc2df3
Sourabh Jain <sourabh.jain@fungible.com>
2020-12-17 17:58:37 +0530

    describing iterable elements of the input blocks

--- log_tools/analysis/blocks/block.py
+++ log_tools/analysis/blocks/block.py
@@ -10,6 +10,15 @@ class Block(object):
         Iterable elements must be a standard 8-tuple:
         (datetime object, time_usecs, system_type, system_id, source_uid, display_time, level, message)
 
+        system_type: Type of system that generated the log lines.
+        (Possible values: fs1600, controller, cluster)
+
+        system_id: Unique identifer of the system that generated the logs.
+        (Possible values: MAC address for fs1600 system, Node ID in case of cluster)
+
+        source_uid: Refers to the software element that generated the logs.
+        (Possible values: apigateway, dataplacement, funos, storage_agent)
+
         The tuple is our poor man's version of a structured log.
 
         Input blocks will ignore iters, output blocks will return None.
