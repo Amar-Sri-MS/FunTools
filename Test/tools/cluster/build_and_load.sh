5fd842eb8d2310774c0343122b35f04304cb0e32
Wangzi He <wangzi.he@fungible.com>
2020-12-19 14:56:36 -0800

    Added support to upload s1/f1 FunOS image.

--- Test/tools/cluster/build_and_load.sh
+++ Test/tools/cluster/build_and_load.sh
@@ -2,29 +2,27 @@
 
 # Before running the script, please make sure to set WORKSPACE to the parent
 # directory containing FunOS and FunSDK.
-# 
+#
 # export WORKSPACE=<path to parent directory containing FunOS/FunSDK>
-# 
-# Run the following command to build FunOS image:
-# 
-# ./build_and_load.sh compile
-# 
-# Run the following command to upload FunOS image to tftpboot server. Once
-# upload is complete, you will be able to find the FunOS image at
-# localadmin@net91-fc-01:/var/lib/tftpboot directory. The FunOS image will be
-# named as <USER-NAME>-funos-s1.signed.gz
-# 
-# ./build_and_load.sh upload
-# 
+#
+# Run the following command to build F1 FunOS image:
+#
+# ./build_and_load.sh --compile f1
+#
+# Run the following command to upload s1 FunOS image to tftpboot server.
+# The FunOS image will be named as <USER-NAME>-funos-<s1/f1>.signed.gz
+#
+# ./build_and_load.sh upload s1
+#
 # Run the following command to start storage initiator with the uploaded
 # FunOS image. This will output FunOS logs and can be piped to a file
 # for debugging.
-# 
-# ./build_and_load.sh run
-# 
-# Run the following command to start a simple FIO test run:
-# 
-# ./build_and_load.sh test 2 True 8589934592
+#
+# ./build_and_load.sh run s1 fc200-9
+#
+# Run the following command to start F1 FunOS image on the FS1600 setup:
+#
+# ./build_and_load.sh run f1 storage-dev-1
 # 
 # The above command will create 2 volumes, each with capacity of 8G and
 # encryption enabled. The two volumes will then be attached to the host server
@@ -53,10 +51,29 @@ clean() {
 }
 
 upload() {
+	if [ -z "$1" ]
+	then
+		echo "Please specify platform type"
+		exit -1
+	fi
+
+	if [ $1 != "s1" ] && [ $1 != "f1" ]
+	then
+		echo "Wrong platform type specified"
+		exit -1
+	fi
+
 	cd $WORKSPACE/FunOS/
-	gzip build/funos-s1.signed
-	mv build/funos-s1.signed.gz build/"$USER"-funos-s1.signed.gz
-	scp build/"$USER"-funos-s1.signed.gz vnc-shared-05:/home/"$USER"/tftpboot
+	if [ $1 == "s1" ]
+	then
+		gzip build/funos-s1.signed
+		mv build/funos-s1.signed.gz build/"$USER"-funos-s1.signed.gz
+		scp build/"$USER"-funos-s1.signed.gz vnc-shared-05:/home/"$USER"/tftpboot
+	else
+		gzip build/funos-f1.signed
+		mv build/funos-f1.signed.gz build/"$USER"-funos-f1.signed.gz
+		scp build/"$USER"-funos-f1.signed.gz vnc-shared-05:/home/"$USER"/tftpboot
+	fi
 }
 
 run() {
@@ -94,30 +111,12 @@ run() {
 	fi
 }
 
-cleanup_testbed() {
-	numvols=0
-	cd $WORKSPACE/py_fabric_27/fs
-	fab -f flib.py cluster_setup:msft1 delete_volumes
-}
-
 test() {
-	cleanup_testbed
-
-	numvols=$1
-	encryption=$2
-	volsize=$3
-	cd $WORKSPACE/py_fabric_27/fs
-	for i in $(seq 1 $numvols);
-	do
-		echo $volsize
-		fab -f flib.py cluster_setup:msft1 create_raw_vol:vol_name="raw-vol-${i}",size="$volsize"
-		fab -f flib.py cluster_setup:msft1 attach_vol_to_host:vol_name="raw-vol-${i}",host_name="cab9-qa-08",via="rds",rno=$i,cno=1,nsid=$i,encrypt=$encryption
-	done
-#ssh localadmin@10.91.0.110 -o StrictHostKeyChecking=no << EOF
-#	cd /home/localadmin/wangzi
-#	echo Fungible/4u | sudo -S ./fio_read_multiple.sh 8 13 $numvols 60
-#	exit
-#EOF
+	if [ ! -d $WORKSPACE/Integration ]
+	then
+		echo "Integration directory does not exist"
+		exit -1
+	fi
 }
 
 fetch_workspace() {
@@ -132,7 +131,7 @@ help_menu() {
 	echo "Usage:"
 	echo -e "--compile s1/f1           Build s1/f1 FunOS image"
 	echo -e "--clean                   Clean up FunOS image"
-	echo -e "--upload                  Upload FunOS image to tftp server"
+	echo -e "--upload s1/f1            Upload FunOS image to tftp server"
 	echo -e "--run s1/f1 setup_name    Run the FunOS image on s1/f1 setup"
 	echo -e "--test <numvols> <encryption> <volsize> Runs a multi-vol test"
 }
@@ -159,8 +158,9 @@ main() {
 		;;
 		--upload)
 		fetch_workspace
-		upload
 		shift # past argument
+		upload $1
+		shift # past value
 		;;
 		--run)
 		fetch_workspace
