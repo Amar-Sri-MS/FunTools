33bcd6c5d490f164b25bf44a2522ff2d146547ed
Eddie Ruan <eddie.ruan@fungible.com>
2020-12-19 18:22:14 -0800

    Add collecting for fm_agent

--- Test/tools/prepare_funos_image_from_dochub.sh
+++ Test/tools/prepare_funos_image_from_dochub.sh
@@ -38,26 +38,29 @@ done
 download_images() {
     cd $TMPLOC
     rm -rf *
-    touch $VERSION
     wget http://dochub.fungible.local/doc/jenkins/master/funsdk/${VERSION}/Linux/funos.mips64-extra.tgz  -q --show-progress
+    wget http://dochub.fungible.local/doc/jenkins/master/funsdk/${VERSION}/build_info.txt -q
 }
 
 create_funos_image() {
     cd $TMPLOC
+    VER=$(cat ./build_info.txt)
+    touch $VER.txt    
     tar xf funos.mips64-extra.tgz
 
     $WORKSPACE/FunSDK/bin/scripts/xdata.py $TMPLOC/bin/funos-s1-release.stripped add $WORKSPACE/FunSDK/bin/cc-linux-yocto/mips64hv/vmlinux.bin
     $WORKSPACE/FunSDK/bin/flash_tools/sign_for_development.py --fourcc fun1 --chip s1 -o $TMPLOC/funos-s1-release.signed $TMPLOC/bin/funos-s1-release.stripped
     gzip $TMPLOC/funos-s1-release.signed
-    cp $TMPLOC/funos-s1-release.signed.gz $FINALLOC
+    filename=funos-s1.signed.gz.${VER}
+    cp $TMPLOC/funos-s1-release.signed.gz $FINALLOC/$filename
 
-    echo "funos-s1-release.signed.gz from version $VERSION at $(date)" >  ${FINALLOC}/funos_image_${VERSION}.txt
+    echo "Built $filename from FunSDK  $VER at $(date)" >  ${FINALLOC}/funos_image_${VER}.txt
     echo ""
+    echo "cat ${FINALLOC}/funos_image_${VER}.txt "
+    cat ${FINALLOC}/funos_image_${VER}.txt
     echo ""
-    ls -l ${FINALLOC}/funos_image_${VERSION}.txt
-    ls -l $FINALLOC/funos-s1-release.signed.gz
-    echo ""
-    cat ${FINALLOC}/funos_image_${VERSION}.txt
+    ls -l $FINALLOC/$filename
+    
 }
 
 download_images
