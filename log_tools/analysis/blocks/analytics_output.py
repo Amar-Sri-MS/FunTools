3662e5b4de02dd2e06cb457cb3d72b16f0e2423c
Sourabh Jain <sourabh.jain@fungible.com>
2021-01-19 16:41:35 +0530

    Support for matching anchors in the AnalyticsOutput block

--- log_tools/analysis/blocks/analytics_output.py
+++ log_tools/analysis/blocks/analytics_output.py
@@ -16,6 +16,158 @@ from urllib.parse import quote_plus
 from blocks.block import Block
 
 
+class AnchorMatch:
+    """ Anchor Match to hold data required for a match """
+
+    def __init__(self, anchor, match, msg_block):
+        """Construct a new AnchorMatch instance.
+
+        Args:
+            anchor (dict): The JSON dictionary that represents the anchor that
+                           the line matched. Expected to have the key "short".
+            match (RE match): The match object.
+            msg_block (dict): The message block which is convereted to dict. Expected
+                        to have the key "datetime"
+        """
+        self.anchor = anchor
+        self.msg_block = msg_block
+
+        # If this is a regular expression anchor, fill all the values in the
+        # short representation with the matchgroups.
+        short = anchor["short"]
+        if "rematch" in anchor:
+            for key, value in match.groupdict().items():
+                short = re.sub(f"<{key}>", value, short)
+        self.short = short
+
+        # Parse the timestamp and store a float version of it, if it's
+        # available.
+        self.matchtime = msg_block['datetime']
+        if self.matchtime:
+            self.matchtime_f = self.matchtime.timestamp()
+        else:
+            self.matchtime_f = None
+
+    # Need this to sort match objects based on float representation of
+    # their timestamp.
+    def __lt__(self, other):
+        return self.matchtime_f < other.matchtime_f
+
+    def __str__(self):
+        logline = ''
+        if self.matchtime:
+            logline += f": [{self.matchtime}]"
+
+        return logline + f": {self.short}"
+
+
+class AnchorMatcher:
+    def __init__(self, anchor_files, anchor_keys, timestamped_only=False):
+        """Construct an AnchorMatcher object. This provides an interface that
+        can return AnchorMatch objects given a filename for a log.
+
+        Args:
+            anchor_files (str list): List of strings that represent filenames
+                                     of files that contain JSON anchors.
+            anchor_keys (set):       List of keys to use from anchors file.
+            timestamped_only (bool): Limit to only matches that have a
+                                     timestamp associated with them.
+        """
+
+        self.timestamped_only = timestamped_only
+        anchors = self._load_anchors(anchor_files, anchor_keys)
+        self.compiled, self.uber_compiled = self._generate_matchers(anchors)
+
+    def _load_anchors(self, anchor_files, anchor_keys):
+        """Generate a list of dictionaries representing the anchors in the
+        given files. Each line in the anchor file will be treated as a separate
+        valid JSON anchor. If the line starts with a '#' character, it will be
+        skipped.
+        """
+
+        anchors = []
+        for anchor_file in anchor_files:
+            print('Reading Anchor file from: ', anchor_file)
+
+            with open(os.path.expandvars(anchor_file)) as f:
+                for line in f:
+                    line = line.strip()
+
+                    # Each anchor is it's own valid piece of JSON. The anchors
+                    # file as a whole is not valid JSON.
+                    if line and not line.startswith("#"):
+                        anchor = json.loads(line)
+                        if anchor_keys == None or anchor["key"] in anchor_keys:
+                            anchors.append(anchor)
+
+        return anchors
+
+    def _generate_matchers(self, anchors):
+        """ Generate the two regular expression matchers. This returns a tuple
+        of (compiled, uber_compiled).
+
+        The compiled list is a list of (anchor, regular expression). This will
+        be used later to narrow down a match after the uber regex has matched.
+
+        The uber_compiled is a single regular expression that is comprised of
+        all of the regular expressions in the compiled list.
+        """
+
+        compiled = []
+        uber_matchers = []
+        for anchor in anchors:
+            if "rematch" in anchor:
+                pattern = anchor["rematch"]
+
+                # Remove the grouping tags of the form ?P<KEY>
+                uber_pattern = re.sub(r"\(\?P<[^>]+>", "(", pattern)
+            else:
+                pattern = anchor["match"]
+                pattern = re.escape(pattern)
+                uber_pattern = pattern
+
+            uber_matchers.append(uber_pattern)
+            compiled.append((anchor, re.compile(pattern)))
+
+        uber_re = "|".join(uber_matchers)
+        uber_compiled = re.compile(uber_re)
+
+        return compiled, uber_compiled
+
+    def _generate_match_entry(self, msg_block):
+        """
+        Find the anchor that generated the match, and create a new AnchorMatch
+        object with the associated message block.
+        """
+
+        line = msg_block['line']
+        for anchor, matcher in self.compiled:
+            match = matcher.search(line)
+            if match:
+                return AnchorMatch(anchor, match, msg_block)
+
+        return None
+
+    def _filter_entry(self, entry):
+        """
+        We have a match entry, now do we want it?
+        """
+
+        return not self.timestamped_only or entry.matchtime
+
+    def generate_match(self, msg_block):
+        """
+        Given a message block, search for match against the given anchors.
+        """
+        line = msg_block['line']
+        if self.uber_compiled.search(line):
+            entry = self._generate_match_entry(msg_block)
+            if entry and self._filter_entry(entry):
+                return entry
+
+        return None
+
+
 class AnalyticsOutput(Block):
     """ Creates HTML pages after analysing the logs """
 
@@ -25,6 +177,7 @@ class AnalyticsOutput(Block):
         # Maintains hash of unique log entry for detecting duplicates.
         # This might take up memory as the log entries increase.
         self.duplicate_entries = {}
+        self.anchor_matches = list()
         self.config = {}
         # Reading config file if available.
         try:
@@ -60,6 +213,13 @@ class AnalyticsOutput(Block):
                                                                                         kibana_selected_columns,
                                                                                         build_id)
 
+        # Check for anchors in log lines with timestamps
+        timestamped_only = True
+
+        anchor_files = cfg.get('anchor_files', [])
+        anchor_keys = cfg.get('anchor_keys', None)
+        self.anchor_matcher = AnchorMatcher(anchor_files, anchor_keys, timestamped_only)
+
     def process(self, iters):
         """ Performs analysis on all iterables """
 
@@ -68,9 +228,12 @@ class AnalyticsOutput(Block):
                 msg_block = self.tuple_to_dict(tuple)
 
                 self.check_for_duplicate_entry(msg_block)
+                self.check_for_anchor_match(msg_block)
 
         # Most duplicated logs
         self.generate_most_duplicates_entries()
+        # Anchors
+        self.store_anchor_matches()
 
     def check_for_duplicate_entry(self, msg_block):
         """ Hashing the current log message to check if exists already """
@@ -130,6 +293,48 @@ class AnalyticsOutput(Block):
 
         path = os.path.join(self.dir, 'duplicates.html')
         self._create_template(path, template_dict)
+
+    def check_for_anchor_match(self, msg_block):
+        match = self.anchor_matcher.generate_match(msg_block)
+        if match:
+            self.anchor_matches.append(match)
+
+    def store_anchor_matches(self):
+        anchors_list = []
+        # Sort the anchor matches by timestamp
+        matches = sorted(self.anchor_matches)
+
+        for match in matches:
+            is_failure = match.anchor.get('is_failure', False)
+            source = match.msg_block.get('uid', 'N/A')
+            system_id = match.msg_block.get('system_id', 'N/A')
+            msg = match.msg_block['line']
+            datetime = match.msg_block['datetime']
+            # Kibana query should be enclosed within quotations for exact match
+            # Removing special characters
+            # TODO(Sourabh): Better approach for handling special characters
+            query = '"{}"'.format(msg.replace('\\','').replace('"',' ').replace('\'', '!\'')).replace('!', '!!')
+            kibana_url = self.kibana_base_url.replace('KIBANA_QUERY', quote_plus(query))
+
+            anchors_list.append({
+                'source': source,
+                'system_id': system_id,
+                'is_failure': is_failure,
+                'link': kibana_url,
+                'datetime': str(datetime),
+                'level': match.msg_block['level'],
+                'msg': match.msg_block['line'],
+                'description': match.short
+            })
+
+        path = os.path.join(self.dir, 'anchors.json')
+        try:
+            # Creating the directory if it does not exist
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            with open(path, 'w+') as f:
+                json.dump(anchors_list, f)
+        except IOError as e:
+            print("I/O error", e)
 
     def _create_template(self, path, template_dict):
         """ Creates a template in the specified path using the template_dict """
--- log_tools/analysis/blocks_test/analytics_output_test.py
+++ log_tools/analysis/blocks_test/analytics_output_test.py
@@ -16,6 +16,17 @@ class AnalyticsOutputTest(unittest.TestCase):
 
     def setUp(self):
         self.block = AnalyticsOutput()
+        cfg = {
+            'env': {
+                'build_id': 'analytics_output_test'
+            },
+            'dir': 'view/analytics/log_${build_id}',
+            'anchor_files': [
+                'config/anchors.json'
+            ],
+            'anchor_keys': None
+        }
+        self.block.set_config(cfg)
 
     def test_check_for_duplicates(self):
         """ check if duplicate log entries are found """
@@ -50,3 +61,25 @@ class AnalyticsOutputTest(unittest.TestCase):
         most_duplicated_lines = [entry['msg']['line'] for entry in most_duplicated_entries]
 
         self.assertListEqual(expected, most_duplicated_lines)
+
+    def test_check_for_anchor_match(self):
+        """ check if anchor matches are found """
+        lines = [
+            '[5.413910 8.0.0][kernel] Welcome to FunOS!',
+            '[1596620459.958822 5.5.2] NOTICE tcp "flow[0x6/0x0/0x000:tcp:tcp_hton_f/0x2/0xa80000002ec86000@FA2:14:1[VP]]: net: aborting: TCP_FSM_STATE_ESTABLISHED, from (remote) 15.149.1.2:1125 to (local) 15.139.2.2:1099 reason 104 (rcvnxt 865474991 rcvwnd 259168 rcvadv 261920 coalesce_plen 0)"'
+        ]
+        expected_count = 2
+        expected_descriptions = [
+            'Welcome to FunOS',
+            'FunTCP connection aborted State: TCP_FSM_STATE_ESTABLISHED Remote: 5.149.1.2:1125 Local 15.139.2.2:1099 Reason: 104'
+        ]
+
+        # Checking for anchor matches
+        for it in lines_to_iterable(lines):
+            msg_block = msg_tuple_to_dict(it)
+            self.block.check_for_anchor_match(msg_block)
+
+        anchor_descriptions = [anchor_match.short for anchor_match in self.block.anchor_matches]
+
+        self.assertEqual(expected_count, len(self.block.anchor_matches))
+        self.assertListEqual(expected_descriptions, anchor_descriptions)
