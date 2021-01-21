ed35d56e85b6d1e1123cb1e03d0c4cb670904c11
Eddie Ruan <eddie.ruan@fungible.com>
2021-01-21 12:56:59 -0800

    Fix

--- Test/tools/update_nfs.sh
+++ Test/tools/update_nfs.sh
@@ -65,6 +65,7 @@ IMAGENAME="${CHIP}-rootfs.tar.xz"
 download_image() {
     mkdir -p $TMPLOC
     cd $TMPLOC
+    rm -f build_info.txt
     rm -f $IMAGENAME
     wget http://dochub.fungible.local/doc/jenkins/master/funsdk/$IMAGEVER/Linux/$IMAGENAME -q --show-progress
     wget http://dochub.fungible.local/doc/jenkins/master/funsdk/$IMAGEVER/build_info.txt -q
