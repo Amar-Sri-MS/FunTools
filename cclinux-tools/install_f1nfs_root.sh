1a82ed690e4eb00e2b9c0a51f764784f5b855308
David Daney <david.daney@fungible.com>
2021-02-04 12:31:51 -0800

    cclinux-tools: Add f1fns things.

---
+++ cclinux-tools/install_f1nfs_root.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+if [ -z "$SDK_INSTALL_DIR" ] ; then
+    echo "Error: SDK_INSTALL_DIR not set."
+    exit 1
+fi
+
+#
+# Tools copied to the deloyment.
+#
+target_dir=$SDK_INSTALL_DIR/bin/scripts
+mkdir -p $target_dir
+
+install -t $target_dir deploy_f1nfs_root.sh
+
