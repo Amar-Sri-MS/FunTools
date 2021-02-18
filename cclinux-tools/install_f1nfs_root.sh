7994294e9145009dad35cb9f84de73dbb593288c
Marcin Nowakowski <marcin.nowakowski@fungible.com>
2021-02-18 07:44:01 +0100

    cclinux: fix dns config handling for fs1600 nfs root
    Signed-off-by: Marcin Nowakowski <marcin.nowakowski@fungible.com>

--- cclinux-tools/install_f1nfs_root.sh
+++ cclinux-tools/install_f1nfs_root.sh
@@ -13,3 +13,8 @@ mkdir -p $target_dir
 
 install -t $target_dir deploy_f1nfs_root.sh
 
+runtime_target_dir=$SDK_INSTALL_DIR/bin/mips64/Linux
+mkdir -p $runtime_target_dir
+
+install -t $runtime_target_dir -m 0644 interfaces-nfs
+install -t $runtime_target_dir -m 0644 vlan-bmc
