a453a4d2d6ba58ba446fd916456fa04cc0109cf7
Sourabh Jain <sourabh.jain@fungible.com>
2021-03-23 16:42:18 +0530

    created a separate js file for web tracking

---
+++ log_tools/analysis/view/static/js/tracker.js
@@ -0,0 +1,26 @@
+// Registering click event handler to track web events
+document.addEventListener('click', function(element) {
+    // Only track elements with the data attribute "web_event"
+    if (element.target.dataset['web_event']) {
+        trackWebEvent(element.target.dataset['web_event']);
+    }
+});
+
+function trackWebEvent(eventName, data=null) {
+    console.log(eventName, data);
+    return;
+    // Create an event object with event name and
+    // timestamp
+    const eventData = {
+        event: eventName,
+        data,
+        timestamp: Date.now(),
+        logID: logId
+    }
+
+    const xhttp = new XMLHttpRequest();
+    const TRACK_WEB_EVENT_API = `${window.location.origin}/events/track`;
+    xhttp.open('POST', TRACK_WEB_EVENT_API);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send(JSON.stringify(eventData));
+}
