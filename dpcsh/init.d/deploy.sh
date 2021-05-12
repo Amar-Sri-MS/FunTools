e78370db137be41294a9496fad241d09b12de877
Renat Idrisov <renat.idrisov@fungible.com>
2021-05-12 11:27:41 -0700

    Adding a DPC over libfunq as optional for some services

--- dpcsh/init.d/deploy.sh
+++ dpcsh/init.d/deploy.sh
@@ -5,8 +5,11 @@ _DPCSH_DEBUG_STARTUP_ID=${DPCSH_DEBUG_STARTUP_ID:-56}
 
 mkdir -p $DEPLOY_ROOT/usr/bin
 install ${DPCSH_INSTALL_DIR}/{dpcsh,dpc_client.py,dpctest.py,dpc_uboot_env.py,auth_firmware.py} $DEPLOY_ROOT/usr/bin
-cp ${DPCSH_INSTALL_DIR}/init.d/{dpc-debug,dpc,dpc-libfunq} ${DEPLOY_ROOT}/etc/init.d
+cp ${DPCSH_INSTALL_DIR}/init.d/{dpc-debug,dpc,dpc-libfunq,dpc-libfunq-debug} ${DEPLOY_ROOT}/etc/init.d
 mkdir -p ${DEPLOY_ROOT}/etc/rc5.d
 cd ${DEPLOY_ROOT}/etc/rc5.d
 ln -sf ../init.d/dpc S${_DPCSH_STARTUP_ID}dpc
 ln -sf ../init.d/dpc-debug S${_DPCSH_DEBUG_STARTUP_ID}dpc-debug
+
+ln -sf ../init.d/dpc-libfunq S${_DPCSH_STARTUP_ID}dpc-libfunq
+ln -sf ../init.d/dpc-libfunq-debug S${_DPCSH_DEBUG_STARTUP_ID}dpc-libfunq-debug
