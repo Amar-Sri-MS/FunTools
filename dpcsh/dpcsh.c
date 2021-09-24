986754ff09d3768a143fcd19d4b453eda2f0d214
Marcin Nowakowski <marcin.nowakowski@fungible.com>
2021-09-24 08:10:11 +0200

    dpcsh: fix transcoding when error node is encountered
    This happens when handling a disconnection of a text client
    
    Signed-off-by: Marcin Nowakowski <marcin.nowakowski@fungible.com>

--- dpcsh/dpcsh.c
+++ dpcsh/dpcsh.c
@@ -467,8 +467,14 @@ static struct fun_ptr_and_size _transcode(struct fun_ptr_and_size source,
 
 	// transcode to text json
 	size_t unused;
-	char *json_text;
-	if (dest->socket->mode == SOCKMODE_TERMINAL) {
+	char *json_text = NULL;
+	struct fun_ptr_and_size result = {};
+
+	if (fun_json_is_error_message(json)) {
+		const char *msg = NULL;
+		fun_json_fill_error_message(json, &msg);
+		log_info("Transcode error: %s\n", msg);
+	} else if (dest->socket->mode == SOCKMODE_TERMINAL) {
 		uint32_t flags = use_hex ? FUN_JSON_PRETTY_PRINT_USE_HEX_FOR_NUMBERS : 0;
 		json_text = fun_json_pretty_print(json,
 							  0, "    ",
@@ -478,9 +484,14 @@ static struct fun_ptr_and_size _transcode(struct fun_ptr_and_size source,
 		json_text = fun_json_to_text_oneline(json, &unused);
 	}
 	fun_json_release(json);
-	size_t json_length = strlen(json_text);
-	struct fun_ptr_and_size result = {.ptr = (uint8_t *)json_text, .size = json_length + 1};
-	json_text[json_length] = '\n';
+
+	if (json_text) {
+		size_t json_length = strlen(json_text);
+		result.ptr = (uint8_t *)json_text;
+		result.size = json_length + 1;
+		json_text[json_length] = '\n';
+	}
+
 	return result;
 }
 
