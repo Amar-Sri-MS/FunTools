f286668e0d903e13bbc566c164694811c53134b3
Sourabh Jain <sourabh.jain@fungible.com>
2021-12-06 19:29:37 +0530

    enable logging from gunicorn workers

--- log_tools/analysis/START_FILE_SERVER.sh
+++ log_tools/analysis/START_FILE_SERVER.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd file_server
-gunicorn --workers 3 --bind 0.0.0.0:11000 wsgi:app
+gunicorn --workers 3 --bind 0.0.0.0:11000 --capture-output --log-level debug wsgi:app
--- log_tools/analysis/START_LOG_ANALYZER.sh
+++ log_tools/analysis/START_LOG_ANALYZER.sh
@@ -4,4 +4,4 @@
 elastic_setup/SETUP_ELASTICSEARCH.sh
 
 cd view
-gunicorn --workers 3 --bind 0.0.0.0:5000 wsgi:app
+gunicorn --workers 3 --bind 0.0.0.0:5000 --capture-output --log-level debug wsgi:app
