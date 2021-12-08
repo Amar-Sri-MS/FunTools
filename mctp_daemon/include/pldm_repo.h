56278a84965e0ac42625e050f3ec2f4139691c1a
Shai E. Lazmi <shai.lazmi@fungible.com>
2021-12-08 15:35:55 -0800

    - Fix pldm repo buildtime (instead of static 2017)

--- mctp_daemon/include/pldm_repo.h
+++
@@ -1,5 +0,0 @@
-#ifndef _INC_PLDM_REPO_HDR_
-#define _INC_PLDM_REPO_HDR_
-struct pldm_timestamp oem_update = {-800, 43, 51, 10, 30, 51, 2021, 1};
-const struct pldm_timestamp repo_update = {-800, 43, 51, 10, 30, 51, 2021, 1};
-#endif /* _INC_PLDM_REPO_HDR_ */
