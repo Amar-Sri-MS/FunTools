7da341555af57b5ad71899b2c4ca17c9bd8e86bb
Anil Kumar <anil.kumar@fungible.com>
2022-01-18 00:06:28 -0800

    nw traffic testing tools

---
+++ ins_cyc/nw/nw_irq_affinity_set.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+import subprocess
+
+def irq_affinity_set(pat, cpu_list):
+  cmd = 'ls -d /proc/irq/*/%s | sort' % pat
+  data = subprocess.check_output(cmd, shell=True)
+  flist = data.strip().splitlines()
+  for i in range(len(flist)):
+    cpu = cpu_list[i % len(cpu_list)]
+    cmd = 'echo %s > %s/smp_affinity_list' % (cpu, flist[i].rsplit('/', 1)[0])
+    print cmd
+    subprocess.check_output(cmd, shell=True)
+    (intf_name, tx_rx, qid) = flist[i].rsplit('/', 1)[1].rsplit('-', 2)
+    if tx_rx == 'tx':
+      core_mask = '%x' % (1 << cpu)
+      if len(core_mask) > 8:
+        loc = len(core_mask) - 8
+        core_mask = core_mask[:loc] + ',' + core_mask[loc:]
+      cmd = 'echo %s > /sys/class/net/%s/queues/%s-%s/xps_cpus' % (core_mask, intf_name, tx_rx, qid)
+      print cmd
+      try:
+        subprocess.check_output(cmd, shell=True)
+      except:
+        pass
+ 
+if __name__ == '__main__':
+    irq_affinity_set('hu1-f1*tx*', range(20, 23))
+    irq_affinity_set('hu1-f1*rx*', [23, 24, 26])
+    irq_affinity_set('hu1-f2*tx*', range(30, 33))
+    irq_affinity_set('hu1-f2*rx*', range(33, 36))
---
+++ ins_cyc/nw/nw_top_host_intr.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import sys
+import subprocess
+import json
+import time
+from collections import OrderedDict
+from tabulate import tabulate
+
+pattern_list = ['-tx-', '-rx-']
+# if you want mellanox
+#pattern_list += ['mlx']
+# if you want eno1
+#pattern_list += ['TxRx']
+
+def intr_info(pat):
+  cmd = 'cat /proc/interrupts | tr -s " "'
+  data = subprocess.check_output(cmd, shell=True)
+  data = data.strip().splitlines()
+  num_cpu = len(data[0].strip().split())
+  data = data[1:]
+  result = OrderedDict()
+  for line in data:
+    line = line.strip().split()
+    intr_id = line[0].split(':')[0]
+    intr_count_list = map(int, line[1:num_cpu+1])
+    other = line[num_cpu + 1:]
+    if other:
+      intr_name = other[-1]
+      for pattern in pattern_list:
+        if pattern in intr_name:
+          k = int(intr_id)
+          result[k] = [intr_name, intr_id, intr_count_list]
+          break
+  return result
+
+def intr_info_delta(res1, res2):
+  res = []
+  for (k, v1) in res1.iteritems():
+    v2 = res2.get(k, None)
+    if not v2:
+      continue
+    v = list(v2)
+    delta_count_list = []
+    for i in range(len(v[2])):
+      count_delta = long(v2[2][i]) - long(v1[2][i])
+      delta_count_list.append(count_delta)
+    delta_count = max(delta_count_list)
+    intr_cpu = delta_count_list.index(delta_count)
+    if delta_count:
+      v = [v2[0], v2[1], v2[2][intr_cpu], '+%s' % delta_count, 'CPU%s' % intr_cpu]
+      res.append(v)
+  hdr=['name', 'intr', 'count', '+count', 'CPU']
+  return (res, hdr)
+
+def intr_funeth(pat):
+  res1 = intr_info(pat)
+  time.sleep(1)
+  res2 = intr_info(pat)
+  (res, hdr) = intr_info_delta(res1, res2)
+  print(tabulate(res, headers=hdr))
+
+def main(argv):
+  mf_flow_dump(argv)
+
+if __name__ == '__main__':
+  intr_funeth(sys.argv)
+
