4fd9f58a38c4cf65d1975af30c4471d20bfa864d
Charles Gray <charles.gray@fungible.com>
2022-01-24 22:01:15 -0800

    Allow missmap to read region info from json peek output

--- csi_miss/region_ident.py
+++ csi_miss/region_ident.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python2.7
 
 import re
+import os
 import sys
 import json
 import argparse
 
 IN_FILE = "uartout0.0.txt"
+IN_JSON_FILE = "regions.js"
 OUT_FILE = "region-ident.js"
 
 ###
@@ -61,10 +63,10 @@ def parse_region_line(line):
     return None
     
 ###
-##  real work
+##  real work for log scraping
 #
 
-def find_regions(fname):
+def find_regions_log(fname):
 
     fl = open(fname)
 
@@ -87,12 +89,60 @@ def find_regions(fname):
     return regions
 
 ###
+##  real work for json scraping
+#
+
+# takes a janky -ve address from json and returns the real number
+def to_addr(n):
+
+    if (n >= 0):
+        return n
+
+    return n + 16**16
+
+# parse input from "peek config/mem_region"
+def find_regions_json(fname):
+
+    regions = []
+
+    # read the input file
+    fl = open(fname)
+    js = json.loads(fl.read())
+
+    for k, d in js.iteritems():
+        start = to_addr(d["start"])
+        end = to_addr(d["start"])
+        t = (start, end, k)
+        regions.append(t)
+
+
+    # done
+    regions.sort()
+    print "found %d regions" % len(regions)
+
+    return regions
+
+###
+##  find any data
+#
+def find_regions(fname_log, fname_json):
+
+    if (os.path.exists(fname_log)):
+        regions = find_regions_log(fname_log)
+    elif (os.path.exists(fname_json)):
+        regions = find_regions_json(fname_json)
+    else:
+        raise RuntimeError("could not find %s or %s" % (fname_log, fname_json))
+
+    return regions
+
+###
 ##  main
 #
 
 def main():
 
-    regions = find_regions(IN_FILE)
+    regions = find_regions(IN_FILE, IN_JSON_FILE)
 
     fl = open(OUT_FILE, "w")
     fl.write(json.dumps(regions, indent=4))
