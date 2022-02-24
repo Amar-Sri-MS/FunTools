f4d5d48a255971e55404f27824cc81f7ac8100d0
karnik.jain <karnik.jain@fungible.com>
2022-02-24 10:07:15 -0800

    - Added mctp_transport support to deploy.sh and Makefile

--- mctp_daemon/init.d/deploy.sh
+++ mctp_daemon/init.d/deploy.sh
@@ -11,10 +11,12 @@ die() {
 
 : ${STARTUP_ID=-90}
 rc_script=mctp_daemon.sh
+mctp_transport_script=mctp_transport_init.sh
 
 mkdir -p $DEPLOY_ROOT/usr/bin
-cp -f $MCTP_DAEMON_INSTALL_DIR/init.d/{$rc_script,mctp_transport_init.sh} $DEPLOY_ROOT/etc/init.d
+cp -f $MCTP_DAEMON_INSTALL_DIR/init.d/{$rc_script,${mctp_transport_script}} $DEPLOY_ROOT/etc/init.d
 
 mkdir -p $DEPLOY_ROOT/etc/rc5.d
 cd ${DEPLOY_ROOT}/etc/rc5.d
 ln -sf ../init.d/$rc_script S${STARTUP_ID}mctp_daemon
+ln -sf ../init.d/${mctp_transport_script} S${STARTUP_ID}mctp_transport_init
