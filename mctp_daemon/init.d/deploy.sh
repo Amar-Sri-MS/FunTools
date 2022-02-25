c7d09825eedb92a3f5ab89ad00cbf0b26da7a927
karnik.jain <karnik.jain@fungible.com>
2022-02-25 10:49:33 -0800

    - Addressed Review comments

--- mctp_daemon/init.d/deploy.sh
+++ mctp_daemon/init.d/deploy.sh
@@ -13,9 +13,9 @@ die() {
 rc_script=mctp_daemon.sh
 
 mkdir -p $DEPLOY_ROOT/usr/bin
-cp -f $MCTP_DAEMON_INSTALL_DIR/init.d/$rc_script $DEPLOY_ROOT/etc/init.d
+cp -f $MCTP_DAEMON_INSTALL_DIR/etc/init.d/$rc_script $DEPLOY_ROOT/etc/init.d
+cp -f $MCTP_DAEMON_INSTALL_DIR/usr/bin/{mctp_daemon,mctp_tmp_agent.sh,mctp_transport.py} $DEPLOY_ROOT/usr/bin
 
 mkdir -p $DEPLOY_ROOT/etc/rc5.d
 cd ${DEPLOY_ROOT}/etc/rc5.d
 ln -sf ../init.d/$rc_script S${STARTUP_ID}mctp_daemon
-ln -sf ../init.d/${mctp_transport_script} S${STARTUP_ID}mctp_transport_init
