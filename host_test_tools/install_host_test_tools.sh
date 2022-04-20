340f530077fd6f6f64381e397f1fd771d24ac810
Robert Bowdidge <robert.bowdidge@fungible.com>
2022-04-20 16:44:05 -0700

    Add file-level description.

--- host_test_tools/install_host_test_tools.sh
+++ host_test_tools/install_host_test_tools.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#
+# Install all the scripts needed for host_test_tools.
+#
+# Because host_test_tools is only used to build other scripts into
+# a deploy image, we only need to install our deploy script which
+# assembles the archive.
 
 cp deploy_host_test_tools.sh $SDK_INSTALL_DIR/bin/scripts
