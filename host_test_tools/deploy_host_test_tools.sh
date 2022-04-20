3bc042b756bb6287e2cfe5e180b91c0e0da2a24d
Robert Bowdidge <robert.bowdidge@fungible.com>
2022-04-20 15:37:02 -0700

    Collect useful scripts for use by Fun-on-demand.

---
+++ host_test_tools/deploy_host_test_tools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Collect files that need to go into host_test_tools component.
+#
+# These binaries and scripts are needed for 
+
+mkdir -p $DEPLOY_ROOT/bin/scripts
+mkdir -p $DEPLOY_ROOT/bin/Linux/x86_64
+
+install bin/mips64/Linux/dpc_client.py $DEPLOY_ROOT/bin/scripts
+
+# TODO(bowdidge):jsonutil, log_tools.
+install bin/scripts/fungdbserver.py $DEPLOY_ROOT/bin/scripts
+
+# TODO: FunTools/fungdbserver/hbm_unshard,ddr_unshard, etc.
+install bin/Linux/x86_64/jsonutil $DEPLOY_ROOT/bin/Linux/x86_64/jsonutil
---
+++ host_test_tools/install_host_test_tools.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp deploy_host_test_tools.sh $SDK_INSTALL_DIR/bin/scripts
