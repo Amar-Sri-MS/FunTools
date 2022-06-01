54f2f4d60c5feb14b7b869a116a6d0a971c5d889
KarnikJain <karnik.jain@fungible.com>
2022-06-01 16:57:48 +0530

    MCTP_Transport GlueLayer Cosmetic Level Changes
    - Changed logging location from /tmp -> /persist/logs as required
    - Changed Debug Levels of desired prints
    - Added Sleep to avoid async event ID conflicts during crash recovery or a restart event
    - Added unsubscribe lines at start of the daemon rather doing inside PCIeVDM Thread

--- mctp_daemon/mctp_transport.py
+++ mctp_daemon/mctp_transport.py
@@ -21,7 +21,7 @@ parser.add_argument( '-log',
                      default='info',
                      help='Provide logging level. Example --loglevel debug, default=info' )
 args = parser.parse_args()
-log.basicConfig(filename='/tmp/mctp_transport.py.log',
+log.basicConfig(filename='/persist/logs/mctp_transport.py.log',
                 level=args.loglevel.upper(),
                 format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
@@ -38,7 +38,7 @@ SMBUS_RX_FIFO = '/tmp/mctp_smbus_rx'
 
 #PLDM/MCTP Packet Egress Path
 def mctp_pcievdm_pkt_tx():
-    log.info("Started a thread to send PLDM/MCTP Pkts to HostServer ...")
+    log.info("Started a thread to send PLDM/MCTP over PCIeVDM Pkts to HostServer ...")
     try:
         mctp_tx_dpc_handle = dpc_client.DpcClient(unix_sock = True)
         with open(PCIEVDM_TX_FIFO, 'rb') as pcievdm_tx_fifo:
@@ -52,12 +52,12 @@ def mctp_pcievdm_pkt_tx():
                     time.sleep(1)
                     continue
 
-                log.debug('Read data from FIFO before Bin -> Blob conversion: %s', mctp_pcievdm_pkt_tx_data)
+                log.info('PLDM/MCTP over PCIeVDM: Read data from FIFO before Bin -> Blob conversion: %s', mctp_pcievdm_pkt_tx_data)
                 data = mctp_tx_dpc_handle.blob_from_string(mctp_pcievdm_pkt_tx_data)
 
-                log.debug('Sending Data to HostServer using FunOS PCIe Driver')
+                log.info('PLDM/MCTP over PCIeVDM: Sending Data to MCTP Master using FunOS PCIe Driver')
                 result = mctp_tx_dpc_handle.execute('mctp_transport', ['mctp_pkt_send', 'mctp_over_pcievdm', ['quote', data]])
-                log.debug("'mctp_pkt_send' packet DPCSH response: {}".format(result))
+                log.info("'mctp_pkt_send' packet DPCSH response: {}".format(result))
 
     except Exception as e:
         pcievdm_tx_fifo.close()
@@ -65,7 +65,7 @@ def mctp_pcievdm_pkt_tx():
         log.exception(" mctp_pcievdm_pkt_tx() failed, error:{}".format(e))
   
 def mctp_smbus_pkt_tx():
-    log.info("Started a thread to send PLDM/MCTP Pkts to HostServer ...")
+    log.info("Started a thread to send PLDM/MCTP Over SMBus Pkts to HostServer ...")
     try:
         mctp_tx_dpc_handle = dpc_client.DpcClient(unix_sock = True)
         with open(SMBUS_TX_FIFO, 'rb') as smbus_tx_fifo:
@@ -79,12 +79,12 @@ def mctp_smbus_pkt_tx():
                     time.sleep(1)
                     continue
 
-                log.debug('Read data from FIFO before Bin -> Blob conversion: %s', mctp_smbus_pkt_tx_data)
+                log.info('PLDM/MCTP Over SMBus: Read data from FIFO before Bin -> Blob conversion: %s', mctp_smbus_pkt_tx_data)
                 data = mctp_tx_dpc_handle.blob_from_string(mctp_smbus_pkt_tx_data)
 
-                log.debug('Sending Data to HostServer using FunOS SMBus Driver')
+                log.info('PLDM/MCTP Over SMBus: Sending data to MCTP Master using FunOS SMBus Driver')
                 result = mctp_tx_dpc_handle.execute('mctp_transport', ['mctp_pkt_send', 'mctp_over_smbus', ['quote', data]])
-                log.debug("'mctp_pkt_send' packet DPCSH response: {}".format(result))
+                log.info("'mctp_pkt_send' packet DPCSH response: {}".format(result))
 
     except Exception as e:
         smbus_tx_fifo.close()
@@ -93,33 +93,30 @@ def mctp_smbus_pkt_tx():
 
 #PLDM/MCTP Packet Ingress Path
 def mctp_pcievdm_pkt_rx():
-    log.info("Started a thread to receive PLDM/MCTP Pkts to HostServer ...")
+    log.info("Started a thread to receive PLDM/MCTP over PCIeVDM Pkts to HostServer ...")
 
     try:
         mctp_rx_dpc_handle = dpc_client.DpcClient(unix_sock = True)
-        #Start a Fresh
-        mctp_rx_dpc_handle.execute("mctp_transport", ['mctp_pkt_recv_unsub', 0])
-        mctp_rx_dpc_handle.execute("mctp_transport", ['mctp_pkt_recv_unsub', 1])
-
+        
         #Subscribe to a MCTP Transport Channel to RX Packets
         tid = mctp_rx_dpc_handle.async_send("mctp_transport", "mctp_pkt_recv_sub")
 
         result = mctp_rx_dpc_handle.async_recv_wait(tid)
-        log.info("MCTP Rx Channel Successfully established: {}".format(result))
+        log.info("PLDM/MCTP Over PCIeVDM Rx Channel Successfully established: {}".format(result))
 
         with open(PCIEVDM_RX_FIFO, 'wb') as pcievdm_rx_fifo:
             log.info("PCIEVDM_RX_FIFO opened")
             while True:
-                log.debug("Waiting for PLDM/MCTP Packet from Host Server ........")
+                log.debug("Waiting for PLDM/MCTP over PCIeVDM Packet from Host Server ........")
                 result = mctp_rx_dpc_handle.async_recv_wait(tid)
-                log.debug("PLDM/MCTP async_recv_wait recieved response: {}".format(result))
+                log.info("PLDM/MCTP Over PCIeVDM: Recieved MCTP Master Request from FunOS: {}".format(result))
 
                 if not result['mctp_over_pcievdm']:
-                    log.debug("pkt != PLDM/MCTP Over PCIeVDM")
+                    log.error("pkt != PLDM/MCTP Over PCIeVDM")
                     continue
 
                 mctp_pcievdm_pkt_rx_data = mctp_rx_dpc_handle.blob_to_string(result['data'])
-                log.debug('Sending PLDM/MCTP Packet to MCTP Daemon for Processing: %s', mctp_pcievdm_pkt_rx_data)
+                log.info('Sending PLDM/MCTP over PCIeVDM Packet to MCTP Daemon for Processing: %s', mctp_pcievdm_pkt_rx_data)
 
                 pcievdm_rx_fifo.write(mctp_pcievdm_pkt_rx_data)
                 #Flush is mandatory for proper writing data to Daemon FIFO
@@ -132,7 +129,7 @@ def mctp_pcievdm_pkt_rx():
         log.exception("mctp_pcievdm_pkt_rx() failed, error:{}".format(e))
 
 def mctp_smbus_pkt_rx():
-    log.info("Started a thread to receive PLDM/MCTP Pkts to HostServer ...")
+    log.info("Started a thread to receive PLDM/MCTP over SMBus Pkts to HostServer ...")
 
     try:
         mctp_rx_dpc_handle = dpc_client.DpcClient(unix_sock = True)
@@ -141,21 +138,21 @@ def mctp_smbus_pkt_rx():
         tid = mctp_rx_dpc_handle.async_send("mctp_transport", "mctp_pkt_recv_sub")
 
         result = mctp_rx_dpc_handle.async_recv_wait(tid)
-        log.info("MCTP Rx Channel Successfully established: {}".format(result))
+        log.info("PLDM/MCTP over SMBus Rx Channel Successfully established: {}".format(result))
 
         with open(SMBUS_RX_FIFO, 'wb') as smbus_rx_fifo:
             log.info("SMBUS_RX_FIFO opened")
             while True:
-                log.debug("Waiting for PLDM/MCTP Packet from Host Server ........")
+                log.debug("PLDM/MCTP Over SMBus: Waiting for PLDM/MCTP Packet from MCTP Master ........")
                 result = mctp_rx_dpc_handle.async_recv_wait(tid)
-                log.debug("PLDM/MCTP async_recv_wait recieved response: {}".format(result))
+                log.info("PLDM/MCTP Over SMBus: Recieved MCTP Master Request from FunOS: {}".format(result))
 
                 if not result['mctp_over_smbus']:
-                    log.debug("pkt != PLDM/MCTP Over SMBus")
+                    log.error("pkt != PLDM/MCTP Over SMBus")
                     continue
 
                 mctp_smbus_pkt_rx_data = mctp_rx_dpc_handle.blob_to_string(result['data'])
-                log.debug('Sending PLDM/MCTP Packet to MCTP Daemon for Processing: %s', mctp_smbus_pkt_rx_data)
+                log.info('Sending PLDM/MCTP over SMBus Packet to MCTP Daemon for Processing: %s', mctp_smbus_pkt_rx_data)
 
                 smbus_rx_fifo.write(mctp_smbus_pkt_rx_data)
                 #Flush is mandatory for proper writing data to Daemon FIFO
@@ -180,16 +177,24 @@ def main():
             else:
                 break
 
+        #Start a Fresh: Must needed, when we recovering from a crash or a restart event
+        mctp_dpc_handle = dpc_client.DpcClient(unix_sock = True)
+        mctp_dpc_handle.execute("mctp_transport", ['mctp_pkt_recv_unsub', 0])
+        mctp_dpc_handle.execute("mctp_transport", ['mctp_pkt_recv_unsub', 1])
+
         log.info("Starting MCTP Transport CCLinux GlueLayer .....")
         # creating mctp_pcievdm_pkt_tx/rx threads
         mctp_pcievdm_pkt_rx_thread = threading.Thread(target=mctp_pcievdm_pkt_rx)
         mctp_pcievdm_pkt_tx_thread = threading.Thread(target=mctp_pcievdm_pkt_tx)
-  
+
         # starting mctp_pcievdm_pkt_rx
         mctp_pcievdm_pkt_rx_thread.start()
         # starting mctp_pcievdm_pkt_tx
         mctp_pcievdm_pkt_tx_thread.start()
-  
+
+        # wait for PCIeVDM RX Thread to finish DPCSH ASYNC Event subscription  
+        time.sleep(1)
+
         # creating mctp_smbus_pkt_tx/rx threads
         mctp_smbus_pkt_rx_thread = threading.Thread(target=mctp_smbus_pkt_rx)
         mctp_smbus_pkt_tx_thread = threading.Thread(target=mctp_smbus_pkt_tx)
@@ -212,7 +217,7 @@ def main():
         log.info("MCTP PKT TX/RX Threads completely executed...")
 
     except Exception as e:
-        log.exception("MCTP Transport Application bailed out due to , error:{}".format(e))
+        log.exception("MCTP Transport GlueLayer bailed out due to , error:{}".format(e))
 
     sys.exit(0)
 
