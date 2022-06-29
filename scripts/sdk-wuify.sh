fec4ee6c0b682232f342a47e9ccca102ba1fc449
Charles Gray <charles.gray@fungible.com>
2022-06-28 18:12:39 -0700

    fix a typo

--- scripts/sdk-wuify.sh
+++ scripts/sdk-wuify.sh
@@ -21,19 +21,19 @@ for fname in "$@"; do
         comby ${args} '#include <generated/wu_ids.h>' '#include <nucleus/wu_register.h>' ${fname}
 
         # CHANNEL() with no attributes
-        comby ${args} 'CHANNEL() void :[name](:[params])' 'CHANNEL_HANDLER(:[name], WU_ATTRS_NONE, :[params])' ${fname}
+        comby ${args} 'CHANNEL() void :[name](:[params])' 'CHANNEL_HANDLER(:[name], WU_ATTR_NONE, :[params])' ${fname}
 
         # CHANNEL(WU_ATTR...) with attributes
         comby ${args} 'CHANNEL(:[[attrs]]) void :[name](:[params])' 'CHANNEL_HANDLER(:[name], :[attrs], :[params])' ${fname}
 
         # CHANNEL_THREAD() with no attributes
-        comby ${args} 'CHANNEL_THREAD() void :[name](:[params])' 'CHANNEL_THREAD(:[name], WU_ATTRS_NONE, :[params])' ${fname}
+        comby ${args} 'CHANNEL_THREAD() void :[name](:[params])' 'CHANNEL_THREAD(:[name], WU_ATTR_NONE, :[params])' ${fname}
 
         # CHANNEL_THREAD(WU_ATTR...) with attributes
         comby ${args} 'CHANNEL_THREAD(:[[attrs]]) void :[name](:[params])' 'CHANNEL_THREAD(:[name], :[attrs], :[params])' ${fname}
 
         # WU_HANDLER() with no attributes
-        comby ${args} 'WU_HANDLER() void :[name](:[params])' 'WU_HANDLER(:[name], WU_ATTRS_NONE, :[params])' ${fname}
+        comby ${args} 'WU_HANDLER() void :[name](:[params])' 'WU_HANDLER(:[name], WU_ATTR_NONE, :[params])' ${fname}
 
         # WU_HANDLER(WU_ATTR...) with no attributes
         comby ${args} 'WU_HANDLER(:[[attrs]]) void :[name](:[params])' 'WU_HANDLER(:[name], :[attrs], :[params])' ${fname}
