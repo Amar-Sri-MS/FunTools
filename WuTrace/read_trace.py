e8fd8fba99a2ab27f574ec9bbcf73b046f86269a
sanya.srivastava <sanya.srivastava@fungible.com>
2022-07-08 13:56:28 -0700

    Reverted wu_trace back to original and necessary comments added

--- WuTrace/read_trace.py
+++ WuTrace/read_trace.py
@@ -265,6 +265,8 @@ class TraceFileParser(object):
         if output_file:
             wfh = open(output_file, 'w')
         # strip 5 initial cluster bytes
+        # TODO: Verify header bytes: starting 4 bytes should be 0xcafe0004
+        # for reference: FunTools/TraceParser/csi_trace.py
         fh.read(5)
 
         while True:
