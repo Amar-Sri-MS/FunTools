46b66e3c4610544205d5fb88c5dd457c9cb48bf8
Charles Gray <charles.gray@fungible.com>
2022-07-25 11:29:11 -0700

    Deploy dpc utils

--- dpcsh/init.d/deploy.sh
+++ dpcsh/init.d/deploy.sh
@@ -4,7 +4,7 @@ _DPCSH_STARTUP_ID=${DPCSH_STARTUP_ID:-55}
 _DPCSH_DEBUG_STARTUP_ID=${DPCSH_DEBUG_STARTUP_ID:-56}
 
 mkdir -p $DEPLOY_ROOT/usr/bin
-install ${DPCSH_INSTALL_DIR}/{dpcsh,dpc_client.py,dpctest.py,dpc_uboot_env.py,binary_json.py,dpc_binary.py,auth_firmware.py} $DEPLOY_ROOT/usr/bin
+install ${DPCSH_INSTALL_DIR}/{dpcsh,dpc_client.py,dpc_utils.py,dpctest.py,dpc_uboot_env.py,binary_json.py,dpc_binary.py,auth_firmware.py} $DEPLOY_ROOT/usr/bin
 cp ${DPCSH_INSTALL_DIR}/init.d/{dpc-debug,dpc} ${DEPLOY_ROOT}/etc/init.d
 mkdir -p ${DEPLOY_ROOT}/etc/rc5.d
 cd ${DEPLOY_ROOT}/etc/rc5.d
