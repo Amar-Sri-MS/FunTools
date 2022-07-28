f2d7b3be6be82456a73dde3f48b7be16dd91012c
sanya.srivastava <sanya.srivastava@fungible.com>
2022-07-27 18:05:22 -0700

    Adding test case for outlier_string()

--- WuTrace/render_test.py
+++ WuTrace/render_test.py
@@ -7,6 +7,7 @@
 import unittest
 
 import render
+import event
 
 class TestDisplay(unittest.TestCase):
     def testTruncatedSecs(self):
@@ -36,6 +37,34 @@ class TestDisplay(unittest.TestCase):
         self.assertEqual('1.001 msec', render.duration_string(1001000))
         self.assertEqual('100.101 msec', render.duration_string(100101000))
         self.assertEqual('23.435678 sec', render.duration_string(23435678000))
+
+    def testOutlierString(self):
+        e1 = event.TraceEvent(1220007424, 1220007424, 'WU1', 'FA0:16:00[VP]')
+        e2 = event.TraceEvent(1, 11, 'WU2', 'FA0:16:00[VP]')
+        e3 = event.TraceEvent(1209000, 1209080, 'WU3', 'FA0:16:00[VP]')
+        e4 = event.TraceEvent(28374832, 28374833, 'WU4', 'FA0:16:00[VP]')
+        e5 = event.TraceEvent(0, 0, 'WU5', 'FA0:16:00[VP]')
+
+        outlier_lis1 = [e1, e2, e3, e4, e5]
+        outlier_lis2 = [e1]
+        outlier_lis3 = []
+
+        result_1 = 'WU1: %s: %s\nWU2: %s: %s\nWU3: %s: %s\nWU4: %s: %s\nWU5: %s: %s'
+        result_1 = result_1 % (render.range_string(1220007424, 1220007424),
+                    render.duration_string(0),
+                    render.range_string(1, 11), render.duration_string(10),
+                    render.range_string(1209000, 1209080),
+                    render.duration_string(80),
+                    render.range_string(28374832, 28374833),
+                    render.duration_string(1),
+                    render.range_string(0, 0), render.duration_string(0))
+        result_2 = 'WU1: %s: %s' % (render.range_string(1220007424, 1220007424),
+                    render.duration_string(0))
+        result_3 = 'There are 0 outliers for this sequence.'
+
+        self.assertEqual(result_1, (render.outlier_string(outlier_lis1)))
+        self.assertEqual(result_2, (render.outlier_string(outlier_lis2)))
+        self.assertEqual(result_3, (render.outlier_string(outlier_lis3)))
 
 class TestPercentile(unittest.TestCase):
     def testEdgeCases(self):
