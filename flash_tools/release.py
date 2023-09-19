7bd8cbd827c8772633473269e288eb7d79d94e9f
Marcin Nowakowski <marcin.nowakowski@fungible.com>
2023-09-19 14:15:59 +0200

    release: generate funvisor-less bundles
    Signed-off-by: Marcin Nowakowski <marcin.nowakowski@fungible.com>

--- flash_tools/release.py
+++ flash_tools/release.py
@@ -90,12 +90,15 @@ ALL_ROOTFS_FILES = {
     # bundle types
     "f1" : [ ('fs1600-rootfs-ro.squashfs', 'f1') ],
     "s1" : [ ('s1-rootfs-ro.squashfs', 's1') ],
-    "f1d1" : [ ('fs1600-rootfs-ro.squashfs', 'f1d1') ]
+    "f1d1" : [ ('fs1600-rootfs-ro.squashfs', 'f1d1') ],
+    "s2" : [ (None, 's2')]
 }
 
 CHIP_SPECIFIC_FILES = {
     # none right now
 }
+
+CHIP_WITH_FUNVISOR = [ 'f1', 'f1d1', 's1' ]
 
 def _rootfs(f, rootfs):
     return '{}.{}'.format(rootfs, f)
@@ -115,6 +118,10 @@ def _mfgnofv(f, signed=False):
 def _nor(f, signed=False):
     return _mfgfilename(f, 'norinstall', signed)
 
+def _want_funvisor(args):
+    return args.funvisor or args.dev_image
+
+
 def main():
     parser = argparse.ArgumentParser()
     config = {}
@@ -128,7 +135,7 @@ def main():
     parser.add_argument('--destdir', default='RELEASE', help='Destination directory for output')
     parser.add_argument('--force-version', type=int, help='Override firmware versions')
     parser.add_argument('--force-description', help='Override firmware description strings')
-    parser.add_argument('--chip', choices=['f1', 's1', 'f1d1'], default='f1', help='Target chip')
+    parser.add_argument('--chip', choices=['f1', 's1', 'f1d1', 's2'], default='f1', help='Target chip')
     parser.add_argument('--debug-build', dest='release', action='store_false', help='Use debug application binary')
     parser.add_argument('--default-config-files', dest='default_cfg', action='store_true')
     parser.add_argument('--dev-image', action='store_true', help='Create a development image installer')
@@ -149,6 +156,8 @@ def main():
 
     if args.extra_funos_suffix:
         funos_suffixes.extend(args.extra_funos_suffix)
+
+    args.funvisor = args.chip in CHIP_WITH_FUNVISOR
 
     args.sdkdir = os.path.abspath(args.sdkdir) # later processing fails if relative path is given
     funos_appname = "funos{}.stripped".format('-'.join(funos_suffixes))
@@ -288,13 +297,14 @@ def main():
         shutil.rmtree('install_tools', ignore_errors=True)
         shutil.copytree(os.path.join(args.sdkdir, 'bin/flash_tools/install_tools'),
             'install_tools')
-        for rootfs, _ in rootfs_files:
-            root_file = os.path.join(args.sdkdir, 'deployments', rootfs)
-            shutil.copy2(root_file, rootfs)
-            try:
-                shutil.copy2(root_file + '.version', rootfs + '.version')
-            except FileNotFoundError:
-                pass
+        if _want_funvisor(args):
+            for rootfs, _ in rootfs_files:
+                root_file = os.path.join(args.sdkdir, 'deployments', rootfs)
+                shutil.copy2(root_file, rootfs)
+                try:
+                    shutil.copy2(root_file + '.version', rootfs + '.version')
+                except FileNotFoundError:
+                    pass
 
         if wanted('prepare'):
             # these files are not needed in sdk-prepare
@@ -335,8 +345,7 @@ def main():
 
             subprocess.check_call(cmd)
 
-        if not args.dev_image:
-            # dev images do not need cclinux stuff
+        if _want_funvisor(args):
             cmd = [ localdir('gen_fgpt.py'), 'fgpt.unsigned' ]
             subprocess.call(cmd)
 
@@ -418,7 +427,7 @@ def main():
                 cmd.extend(['--blob', 'csr_override.bjson.signed'])
         subprocess.check_call(cmd)
 
-        if not args.dev_image:
+        if _want_funvisor(args):
             for rootfs, _ in rootfs_files:
                 cmd = [ localdir('gen_hash_tree.py'),
                         '-O', _rootfs('fvht.bin', rootfs),
@@ -442,23 +451,31 @@ def main():
         os.chdir(args.destdir)
         tarfiles = []
 
+        def skip_tarfile_entry(e):
+            if e.get("no_export", False):
+                return True
+            if e.get("only_with_funvisor", False):
+                return not _want_funvisor(args)
+            return False
+
         with open("image.json") as f:
             images = json.load(f)
             tarfiles.extend([key for key,value in images['signed_images'].items()
-                                if not value.get("no_export", False)])
+                                if not skip_tarfile_entry(value)])
             tarfiles.extend([key for key,value in images['signed_meta_images'].items()
-                                if not value.get("no_export", False)])
+                                if not skip_tarfile_entry(value)])
 
         with open("mmc_image.json") as f:
             images = json.load(f)
             tarfiles.extend([key for key,value in images['generated_images'].items()
-                                if not value.get("no_export", False)])
+                                if not skip_tarfile_entry(value)])
 
         tarfiles.append('image.json')
         tarfiles.append('mmc_image.json')
-        for rootfs, _ in rootfs_files:
-            tarfiles.append(_rootfs('fvht.bin', rootfs))
-            tarfiles.append(rootfs)
+        if _want_funvisor(args):
+            for rootfs, _ in rootfs_files:
+                tarfiles.append(_rootfs('fvht.bin', rootfs))
+                tarfiles.append(rootfs)
         tarfiles.extend(glob.glob('qspi_image_hw.bin.*'))
 
         for chip_file in chip_specific_files:
@@ -474,32 +491,32 @@ def main():
         os.chdir(curdir)
 
     if wanted('bundle'):
-        os.chdir(args.destdir)
-        for rootfs, bundle_target in rootfs_files:
+        def skip_bundle_entry(e):
+            if e.get("no_export", False):
+                return True
+            if e.get("no_bundle", False):
+                return True
+            if e.get("only_with_funvisor", False):
+                return not _want_funvisor(args)
+            return False
+
+        def bundle_gen(rootfs, bundle_target, suffix):
+            os.chdir(args.destdir)
             shutil.rmtree('bundle_installer', ignore_errors=True)
             os.mkdir('bundle_installer')
             bundle_images = []
 
-            def skip_entry(e):
-                if e.get("no_export", False):
-                    return True
-                if e.get("no_bundle", False):
-                    return True
-                if args.dev_image and e.get("skip_in_dev_bundle", False):
-                    return True
-                return False
-
             with open("image.json") as f:
                 images = json.load(f)
                 bundle_images.extend([key for key,value in images.get('signed_images',{}).items()
-                                    if not skip_entry(value)])
+                                    if not skip_bundle_entry(value)])
                 bundle_images.extend([key for key,value in images.get('signed_meta_images', {}).items()
-                                    if not skip_entry(value)])
+                                    if not skip_bundle_entry(value)])
 
             with open("mmc_image.json") as f:
                 images = json.load(f)
                 bundle_images.extend([key for key,value in images.get('generated_images',{}).items()
-                                    if not skip_entry(value)])
+                                    if not skip_bundle_entry(value)])
 
             bundle_images.extend([
                 'install_tools/run_fwupgrade.py',
@@ -507,20 +524,20 @@ def main():
                 'image.json'
             ])
 
-            if not args.dev_image:
+            if _want_funvisor(args):
                 bundle_images.extend([
                     _rootfs('fvht.bin', rootfs),
                     rootfs
                 ])
+
+                if os.path.exists(rootfs + '.version'):
+                    bundle_images.append(rootfs + '.version')
 
             for chip_file in chip_specific_files:
                 bundle_images.append(os.path.basename(chip_file))
 
             if os.path.exists('.version'):
                 bundle_images.append('.version')
-
-            if os.path.exists(rootfs + '.version'):
-                bundle_images.append(rootfs + '.version')
 
             for f in bundle_images:
                 os.symlink(os.path.join(os.path.abspath(os.curdir), f), os.path.join('bundle_installer', os.path.basename(f)))
@@ -530,7 +547,8 @@ def main():
                     'ROOTFS_NAME="{}"\n'.format(rootfs),
                     'CHIP_NAME="{}"\n'.format(args.chip.upper()),
                     'DEV_IMAGE={}\n'.format(1 if args.dev_image else 0),
-                    'SDK_BUNDLE={}\n'.format(1 if config_sdk_package else 0)
+                    'SDK_BUNDLE={}\n'.format(1 if config_sdk_package else 0),
+                    'WITH_FUNVISOR={}\n'.format(1 if args.funvisor else 0)
                 ])
 
             bundle_name = 'development_image' if args.dev_image else bundle_target
@@ -538,14 +556,26 @@ def main():
                 os_utils.path_fixup('makeself'),
                 '--follow',
                 'bundle_installer',
-                f'setup_bundle_{bundle_funos_type}{bundle_name}.sh',
-                f'CCLinux/FunOS {args.funos_type} {bundle_name} installer',
+                f'setup_bundle_{bundle_funos_type}{bundle_name}{suffix}.sh',
+                f'{"CCLinux/" if args.funvisor else ""}FunOS {args.funos_type} {bundle_name} installer',
                 './setup.sh'
             ]
 
             subprocess.call(makeself)
+            os.chdir(curdir)
 
-        os.chdir(curdir)
+        fv = args.funvisor
+        for rootfs, bundle_target in rootfs_files:
+            # List of tuples (use_funvisor, suffix) to generate bundles
+            # For DPUs with funvisor support add an extra '_nofv' suffix to bundles
+            # and preserve default name for funvisor bundles.
+            fv_opts = [(False, '_nofv'), (True, '')] if args.chip in CHIP_WITH_FUNVISOR \
+                    else [(False, '')]
+            for fv_opt, suffix in fv_opts:
+                args.funvisor = fv_opt
+                bundle_gen(rootfs, bundle_target, suffix)
+        args.funvisor = fv
+
 
     if wanted('eeprbundle'):
         os.chdir(args.destdir)
@@ -672,7 +702,8 @@ def main():
             gf.create_file(outname_modifier(funos_appname, signed=True), section='signed_mfg_images')
 
 
-        _gen_xdata_funos(_mfg, with_fv=True)
+        if args.chip in CHIP_WITH_FUNVISOR:
+            _gen_xdata_funos(_mfg, with_fv=True)
         _gen_xdata_funos(_mfgnofv, with_fv=False)
         _gen_xdata_funos(_nor, 'nor')
 
@@ -682,8 +713,9 @@ def main():
         os.chdir(args.destdir)
         tarfiles = []
 
+        mod = _mfg if args.chip in CHIP_WITH_FUNVISOR else _mfgnofv
         tarfiles.extend(glob.glob('qspi_image_hw.bin.*'))
-        tarfiles.append(_mfg(funos_appname, signed=True))
+        tarfiles.append(mod(funos_appname, signed=True))
 
         if os.path.exists('.version'):
             tarfiles.append('.version')
