Amar-Sri-MS/FunTools

commit 12ba2752fd5fe0d35db1f1905a0e121d8686b001

Support for board config

flash_tools/install_tools/setup.sh
@@ -187,6 +187,12 @@ else
 				log_msg "This sku \"$host_sku\" is not supported by this bundle"
 				exit 1
 			fi
+			./run_fwupgrade.py ${FW_UPGRADE_ARGS} -u bcfg --check-image-only --select-by-image-type "board_cfg_${host_sku}_default"
+			RC=$?; [ $EXIT_STATUS -eq 0 ] && [ $RC -ne 0 ] && EXIT_STATUS=$RC # only set EXIT_STATUS to error on first error
+			if [ $EXIT_STATUS -ne 0 ]; then
+				log_msg "This sku \"$host_sku\" is not supported by this bundle"
+				exit 1
+			fi
 		fi
 		./run_fwupgrade.py ${FW_UPGRADE_ARGS} -U --version latest --force --downgrade
 		RC=$?; [ $EXIT_STATUS -eq 0 ] && [ $RC -ne 0 ] && EXIT_STATUS=$RC # only set EXIT_STATUS to error on first error
@@ -230,7 +236,14 @@ else
 			./run_fwupgrade.py ${FW_UPGRADE_ARGS} -u eepr --version latest --force --downgrade --select-by-image-type "$host_sku"
 			RC=$?; [ $EXIT_STATUS -eq 0 ] && [ $RC -ne 0 ] && EXIT_STATUS=$RC # only set EXIT_STATUS to error on first error
 
-			./run_fwupgrade.py ${FW_UPGRADE_ARGS} -u eepr --version latest --force --downgrade --active --select-by-image-type "$host_sku"
+			./run_fwupgrade.py ${FW_UPGRADE_ARGS} -u eepr --version latest --force --downgrade --active --select-by_image-type "$host_sku"
+			RC=$?; [ $EXIT_STATUS -eq 0 ] && [ $RC -ne 0 ] && EXIT_STATUS=$RC # only set EXIT_STATUS to error on first error
+
+			log_msg "Downgrading bcfg \"$host_sku\""
+			./run_fwupgrade.py ${FW_UPGRADE_ARGS} -u bcfg --version latest --force --downgrade --select-by-image-type "board_cfg_${host_sku}_default"
+			RC=$?; [ $EXIT_STATUS -eq 0 ] && [ $RC -ne 0 ] && EXIT_STATUS=$RC # only set EXIT_STATUS to error on first error
+
+			./run_fwupgrade.py ${FW_UPGRADE_ARGS} -u bcfg --version latest --force --downgrade --active --select-by-image-type "board_cfg_${host_sku}_default"
 			RC=$?; [ $EXIT_STATUS -eq 0 ] && [ $RC -ne 0 ] && EXIT_STATUS=$RC # only set EXIT_STATUS to error on first error
 		fi
 	else
@@ -239,6 +252,13 @@ else
 
 		if [ $EXIT_STATUS -eq 2 ]; then
 			log_msg "Aborting ... downgrade argument required"
+			# exit early here, as this error code means no upgrade
+			# was performed by run_fwupgrade script
+			exit $EXIT_STATUS
+		fi
+
+		if [ $EXIT_STATUS -eq 3 ]; then
+			log_msg "Aborting upgrade!"
 			# exit early here, as this error code means no upgrade
 			# was performed by run_fwupgrade script
 			exit $EXIT_STATUS
@@ -265,13 +285,16 @@ else
 			dpcsh -Q boot_partition clear
 		fi
 
-		# skip eeprom update on sdk bundles, as eepr images are not present
+		# skip eeprom and bcfg update on sdk bundles, as eepr images are not present
 		if [ -n "$host_sku" ] && [ "$SDK_BUNDLE" -eq 0 ]; then
 			log_msg "Updating eepr \"$host_sku\""
 			./run_fwupgrade.py ${FW_UPGRADE_ARGS} -u eepr --select-by-image-type "$host_sku"
 			RC=$?; [ $EXIT_STATUS -eq 0 ] && [ $RC -ne 0 ] && EXIT_STATUS=$RC # only set EXIT_STATUS to error on first error
-		fi
 
+			log_msg "Updating bcfg \"$host_sku\""
+			./run_fwupgrade.py ${FW_UPGRADE_ARGS} -u bcfg --select-by-image-type "board_cfg_${host_sku}_default"
+			RC=$?; [ $EXIT_STATUS -eq 0 ] && [ $RC -ne 0 ] && EXIT_STATUS=$RC # only set EXIT_STATUS to error on first error
+		fi
 	fi
 fi
 

boardcfgutil/Makefile
@@ -1,0 +1,60 @@
+##
+##  Makefile
+##
+##  Created by Nag Ponugoti on 2023-10-10.
+##  Inherited from Charles Gray's commit 213ff228a
+##  Copyright (c) 2023 Microsoft. All rights reserved.
+##
+
+# OS
+OS := $(shell uname)
+
+# arch
+ARCH := $(shell uname -m)
+
+SDKDIR ?= ../../FunSDK
+SDK_INSTALL_DIR ?= $(SDKDIR)
+
+all: boardcfgutil 
+
+# define header and library location in FunSDK
+CFLAGS ?= -I $(SDKDIR)/FunSDK/funosrt/include  \
+	  -I $(SDKDIR) -Wall -Werror
+
+# XXX: compatibility with FunOS build parameters
+LDFLAGS_ASAN ?= -fsanitize=address
+ifeq ($(OS),Linux)
+#asan library linking does not work for Linux
+LDFLAGS += -L $(SDKDIR)/lib/$(OS)/noasan/$(ARCH) -lfunclient -lm -ldl
+DARWIN_ASAN_FIXUP :=
+else
+LDFLAGS += -L $(SDKDIR)/lib/$(OS)/$(ARCH) -lfunclient -lm $(LDFLAGS_ASAN)
+endif
+
+SRC += boardcfgutil.c
+
+boardcfgutil: boardcfgutil.c
+	$(CC) -o boardcfgutil $(SRC) $(CFLAGS) $(LDFLAGS)
+
+ifeq ($(OS),Darwin)
+# FF: since the path and the compatibility of the ASAN library keeps changing,
+# find the copy used to build the jsonutil and copy it into the same directory
+# as the jsonutil exectuable so it can always find it.
+# $ otool -L jsonutil
+#jsonutil:
+#	/usr/lib/libSystem.B.dylib (compatibility version 1.0.0, current version 1252.200.5)
+#	@rpath/libclang_rt.asan_osx_dynamic.dylib (compatibility version 0.0.0, current version 0.0.0)
+#Load command 14
+#          cmd LC_RPATH
+#      cmdsize 32
+#         path @executable_path (offset 12)
+DARWIN_ASAN_FIXUP := find /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/ -name libclang_rt.asan_osx_dynamic.dylib -exec cp {} $(SDK_INSTALL_DIR)/bin/$(OS)/$(ARCH) \;
+endif
+
+install: boardcfgutil 
+	mkdir -p $(SDK_INSTALL_DIR)/bin/$(OS)/$(ARCH)
+	cp boardcfgutil $(SDK_INSTALL_DIR)/bin/$(OS)/$(ARCH)
+	$(DARWIN_ASAN_FIXUP)
+
+clean:
+	-rm -f boardcfgutil 

flash_tools/build_nor_image.py
@@ -76,13 +76,13 @@ DEFAULT_EEPROMS = {
     ('f1', False)   : 'eeprom_f1_fs800',
 
     ('s1', True)    : 'eeprom_emu_s1_compute',
-    ('s1', False)   : 'eeprom_s1_fc200_1_0GHz_SoC',
+    ('s1', False)   : 'eeprom_s1_fc200',
 
-    ('f1d1', True)  : 'eeprom_emu_f1d1_compute_ddr',
+    ('f1d1', True)  : 'eeprom_emu_f1d1_compute_hbm',
     ('f1d1', False) : 'eeprom_f1d1_fs800v2_1_1GHz_SoC_1_7GHz_PC',
 
     ('s2', True)    : 'eeprom_emu_s2_compute',
-    ('s2', False)   : 'eeprom_s2_qemu',      #FIXME WHEN SILICON
+    ('s2', False)   : 'eeprom_ds400',      #FIXME WHEN SILICON
 }
 
 def remove_prefix(a_str, prefix):
@@ -96,13 +96,14 @@ def compress_file(src_path):
         with gzip.open(src_path + '.gz', 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
 
-def generate_dynamic_config(chip, eeprom):
+def generate_dynamic_config(chip, eeprom, board_cfg):
     ''' generate on the fly a config string specifying the eeprom and the host '''
     uboot_rel_dir = "FunSDK/FunSDK/u-boot/" + chip + "/u-boot.bin"
     host_location = os.path.join(os.environ["WORKSPACE"], uboot_rel_dir)
 
     eeprom_source = {"source" : eeprom}
-    signed_images = {"eeprom_packed.bin" : eeprom_source}
+    board_cfg_source = {"source" : board_cfg}
+    signed_images = {"eeprom_packed.bin" : eeprom_source, "board_cfg.bin" : board_cfg_source}
     host_source = {"source" : host_location}
     signed_images["host_firmware_packed.bin"] = host_source
     top_dir = {"signed_images" : signed_images}
@@ -143,10 +144,8 @@ def generate_nor_image(args, script_directory, images_directory, *dirs):
     # stdin input will be used to send the config that was generated on the fly
     run_args.append("-")
 
-    print(run_args)
-
     # generate a config string on the fly: host for the chip, eeprom
-    extra_config = generate_dynamic_config(args.chip, args.eeprom)
+    extra_config = generate_dynamic_config(args.chip, args.eeprom, args.board_profile)
 
     subprocess.run(run_args, input=extra_config, cwd=images_directory,
                    check=True, stdout=sys.stdout, stderr=sys.stderr)
@@ -154,6 +153,45 @@ def generate_nor_image(args, script_directory, images_directory, *dirs):
     # compress the Nor image file preemptively
     full_nor_image_path = os.path.join(images_directory, NOR_IMAGE_FILE_NAME)
     compress_file(full_nor_image_path)
+
+
+def generate_board_cfg_signed_images(script_directory, images_directory,
+                                     board_cfg_directory, version,
+                                     board_cfg_profile):
+    ''' sign all the board cfg profiles and place them in the images_directory '''
+    #  fixed arguments
+    run_args = ["python3",
+                os.path.join(script_directory, "generate_flash.py"),
+                "--fail-on-error",
+                "--action", "sign",
+                "--config-type", "json",
+                "--source-dir", board_cfg_directory]
+
+    # optional arguments
+    if version:
+        run_args.extend(["--force-version", version])
+
+    # stdin input will be used to send the config that was generated on the fly
+    run_args.append("-")
+
+    extra_config = b'''
+    {
+        "signed_images": {
+        "board_cfg_list": {
+            "source": "%s",
+            "version": 1,
+            "fourcc": "bcfg",
+            "key": "fpk3",
+            "cert": "",
+            "customer_cert": "",
+            "description": ""
+        }
+    }
+    }
+    ''' % board_cfg_profile.encode('utf-8') if board_cfg_profile else b"@file:boardcfg_profile_list.json"
+
+    subprocess.run(run_args, input=extra_config, cwd=images_directory,
+                   check=True, stdout=sys.stdout, stderr=sys.stderr)
 
 
 def generate_eeprom_signed_images(script_directory, images_directory,
@@ -309,11 +347,15 @@ def parse_args():
                             help="Machine (f1,s1,etc...), default = f1")
     arg_parser.add_argument("-e", "--eeprom", action='store',
                             help="eeprom type")
+    arg_parser.add_argument("--board-profile", action='store',
+                            help="board profile")
     arg_parser.add_argument("--no-recompile", action='store_false',
                             dest="recompile",
                             help="Do not compile software, just build image")
     arg_parser.add_argument("--sign-all-eeproms", action='store_true',
                             help="Sign all eeproms -- legacy behavior")
+    arg_parser.add_argument("--sign-all-board-cfg-profiles", action='store_true',
+                            help="Sign all board cfg profiles")
     arg_parser.add_argument("--emulation", action='store_const', const=1, default=0,
                             help="emulation_build")
     arg_parser.add_argument("-n", "--enrollment-certificate", action='store',
@@ -361,7 +403,7 @@ def parse_args():
     return arg_parser.parse_args()
 
 
-def sanitize_args(args, eeproms_dir):
+def sanitize_args(args, eeproms_dir, board_cfg_dir):
     ''' sanitize the args and provide meaningful defaults '''
 
     # can we scp?
@@ -378,6 +420,7 @@ def sanitize_args(args, eeproms_dir):
     # eeprom
     # verify the file exists; if not show the list
     eeprom_files = sorted(os.listdir(eeproms_dir))
+    board_cfg_files = sorted(os.listdir(board_cfg_dir))
 
     if args.eeprom is None:
         try:
@@ -415,6 +458,28 @@ def sanitize_args(args, eeproms_dir):
 
         args.eeprom = sane_eeprom
 
+    sku_name = remove_prefix(args.eeprom, "eeprom_")
+    if args.board_profile is None:
+        args.board_profile = 'boardcfg_{}_default'.format(sku_name)
+        if not args.board_profile in board_cfg_files:
+            sku_name = remove_prefix(sku_name, args.chip + "_")
+            args.board_profile = 'boardcfg_{}_default'.format(sku_name)
+    else:
+        prof_name = remove_prefix(args.board_profile, 'boardcfg_')
+        if not prof_name.startswith(sku_name):
+            prof_name = args.chip + '_' + prof_name
+            if not prof_name.startswith(sku_name):
+                print("board cfg profile: {} for the sku: {} is invalid!".format(prof_name, sku_name))
+                sys.exit(1)
+
+    # verify the file exists; if not show the list
+    if not args.board_profile in board_cfg_files:
+        print("board_profile name is not found: \"{0}\"".format(args.board_profile))
+        print("Available board profiles are:")
+        print("\n".join(board_cfg_files))
+        sys.exit(1)
+
+
     # enrollment certificate
     if args.enrollment_certificate:
         args.enrollment_certificate = os.path.abspath(args.enrollment_certificate)
@@ -434,17 +499,20 @@ def sanitize_args(args, eeproms_dir):
 
     return args
 
-
 def main():
-
     ''' main '''
+    args = parse_args()
     script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
     eeproms_dir = os.path.join(os.environ['WORKSPACE'],
                                'FunSDK/FunSDK/dpu_eepr')
     sbus_roms_dir = os.path.join(os.environ['WORKSPACE'],
                                'FunSDK/FunSDK/sbpfw/roms')
 
-    args = sanitize_args(parse_args(), eeproms_dir)
+    board_cfg_dir = os.path.join(os.environ['WORKSPACE'],
+                                'FunSDK/feature_sets/boardcfg/{}{}'.format(
+			                        args.chip, '-emu' if args.emulation else ''))
+
+    args = sanitize_args(parse_args(), eeproms_dir, board_cfg_dir)
 
     BUILD_DIR_FORMAT = "{sbp}/{build_dir}_{chip}_{emulation}{debug}"
 
@@ -467,8 +535,12 @@ def main():
                                   eeproms_dir, args.version,
                                   None if args.sign_all_eeproms else args.eeprom)
 
+    generate_board_cfg_signed_images(script_dir, built_images_dir,
+                                  board_cfg_dir, args.version,
+                                  None if args.sign_all_board_cfg_profiles else args.board_profile)
+
     # now generate a NOR image
-    generate_nor_image(args, script_dir, built_images_dir, eeproms_dir, sbus_roms_dir)
+    generate_nor_image(args, script_dir, built_images_dir, eeproms_dir, sbus_roms_dir, board_cfg_dir)
 
     print("*** Images in {0} ***".format(built_images_dir))
 

flash_tools/generate_flash.py
@@ -561,7 +561,8 @@ def main():
     else:
         for config_file in args.config:
             if config_file == '-':
-                merge_configs(config, json.load(sys.stdin))
+                x = json.load(sys.stdin)
+                merge_configs(config, x)
             else:
                 with open(config_file, 'r') as f:
                     merge_configs(config, json.load(f))
@@ -625,7 +626,7 @@ def run(arg_action, arg_enroll_cert = None, *args, **kwargs):
                         new_entries[fname] = new_v
             except:
                 print("Skipping generation of {}, input file {} not found".format(
-                        k, v['source'][len('@file:'):]))
+                    k, v['source'][len('@file:'):]))
 
             delete_entries.append(k)
 

flash_tools/install_tools/run_fwupgrade.py
@@ -80,6 +80,7 @@ SPLIT_SIZE = 2*1024*1024
 EXIT_CODE_OK = 0
 EXIT_CODE_ERROR = 1
 EXIT_CODE_DOWNGRADE_ATTEMPT = 2
+EXIT_CODE_UPGRADE_ATTEMPT = 3
 
 class UpgradeStatus(Enum):
     STARTED = 0
@@ -93,6 +94,9 @@ class UpgradeException(Exception):
     pass
 
 class DowngradeAttemptException(UpgradeException):
+    pass
+
+class UpgradeAttemptException(UpgradeException):
     pass
 
 class UpgradeFailedException(UpgradeException):
@@ -193,6 +197,16 @@ def _sbpfw_fourccs_fixup(fourccs):
 
     return fourccs
 
+def check_upgrade_compatibility(cur, to, fourcc, status):
+    # bcfg fourcc is not recognized by pufr and frmw versions older to 20738
+    if (fourcc == 'frmw' or fourcc == 'pufr' or fourcc == 'sbpf') and\
+        cur < 20738 and to > 20738  and status in ['active', 'unknown']:
+        print(f"\nIncompatible {fourcc} upgrade({cur} -> {to}) status: {status}!"\
+               " Upgrade to version 20738 before upgrading to version higher than 20738.")
+        return False;
+
+    return True
+
 
 def run_upgrade(args, release_images):
     """
@@ -244,6 +258,9 @@ def run_upgrade(args, release_images):
                 if fw['fourcc'] == 'mmc1' and fw['version'] < 11500 and \
                     fw.get('status','active') in ['active', 'unknown']:
                     release_images_fourccs = release_images_fourccs - ASYNC_ONLY_IMAGES
+
+                if not check_upgrade_compatibility(fw['version'], v, fw['fourcc'], fw.get('status','active')):
+                    raise UpgradeAttemptException()
 
                 # firmwares older than 9531 do not recognize kbag or husc as
                 # valid identifiers, so do not attempt to program these images, as this
@@ -307,6 +324,8 @@ def run_upgrade(args, release_images):
     else:
         for fourcc in set(args.upgrade):
             for fw in fwinfo['firmwares']:
+                if not check_upgrade_compatibility(fw['version'], v, fourcc, fw.get('status','active')):
+                    raise UpgradeAttemptException()
                 if fourcc_eq(fw['fourcc'], fourcc):
                     if fw['version'] < v or fw['fourcc'] in IGNORE_VERSION_IMAGES:
                         dev_upgrade_fourccs.append(fourcc)
@@ -533,10 +552,11 @@ def main():
         if args.check_image_only:
             i = { k:release_images.get(k) for k in args.upgrade if release_images.get(k) }
             sys.exit(EXIT_CODE_OK if len(i) == len(args.upgrade) else EXIT_CODE_ERROR)
-
         run_upgrade(args, release_images)
     except DowngradeAttemptException:
         sys.exit(EXIT_CODE_DOWNGRADE_ATTEMPT)
+    except UpgradeAttemptException:
+        sys.exit(EXIT_CODE_UPGRADE_ATTEMPT)
     except Exception as e:
         print(f"Upgrade error ... {e}")
         traceback.print_exc()

flash_tools/release.py
@@ -41,6 +41,23 @@ EEPROM_CONFIG_OVERRIDE="""
 }
 """
 
+BOARD_CFG_PER_CHIP_SKU_DEFAULT = {
+	"f1": "f1_dev_board",
+	"f1d1": "fs800v2",
+	"s1": "fc50",
+	"s2": "ds400"
+}
+
+# These are dummy filler configs. Upgrade script picks the right one based on the sku
+BOARD_CONFIG_OVERRIDE="""
+{{ "signed_images": {{
+     "board_cfg.bin": {{
+         "source":"boardcfg_{sku_name}_default"
+         }}
+     }}
+}}
+"""
+
 FUNOS_CONFIG_OVERRIDE="""
 {{ "signed_images": {{
      "funos.signed.bin": {{
@@ -54,6 +71,15 @@ EEPROM_LIST_CONFIG_OVERRIDE="""
 {{ "signed_images": {{
      "eeprom_list": {{
          "source":"@file:{eeprom_list}"
+         }}
+     }}
+}}
+"""
+
+BOARD_CFG_LIST_CONFIG_OVERRIDE="""
+{{ "signed_images": {{
+     "board_cfg_list": {{
+         "source":"@file:{board_cfg_list}"
          }}
      }}
 }}
@@ -192,7 +218,12 @@ def main():
                 gf.merge_configs(config, json.load(f))
 
     eeprom_list = '{}_eeprom_list.json'.format(args.chip)
+    board_cfg_list = 'boardcfg_profile_list.json'
     fvht_list_file = None
+    default_board =  BOARD_CFG_PER_CHIP_SKU_DEFAULT.get(args.chip)
+    if not default_board:
+        print('Unsupported chip: {}!'.format(args.chip))
+        exit(1)
 
     if wanted('prepare') or wanted('sdk-prepare'):
         gf.merge_configs(config, json.loads(EEPROM_CONFIG_OVERRIDE), only_if_present=True)
@@ -200,6 +231,8 @@ def main():
         gf.merge_configs(config, json.loads(FUNOS_CONFIG_OVERRIDE.format(funos_appname=funos_appname)), only_if_present=True)
         gf.merge_configs(config, json.loads(EEPROM_LIST_CONFIG_OVERRIDE.format(eeprom_list=eeprom_list)), only_if_present=True)
         gf.merge_configs(config, json.loads(CSRR_CONFIG_OVERRIDE.format(chip=args.chip)), only_if_present=True)
+        gf.merge_configs(config, json.loads(BOARD_CFG_LIST_CONFIG_OVERRIDE.format(board_cfg_list=board_cfg_list)), only_if_present=True)
+        gf.merge_configs(config, json.loads(BOARD_CONFIG_OVERRIDE.format(sku_name=default_board)), only_if_present=True)
 
         if not args.with_csrreplay:
             try:
@@ -264,6 +297,7 @@ def main():
         paths_chip_specific = [ "FunSDK/u-boot/{chip}",
                 "FunSDK/sbpfw/firmware/chip_{chip}_emu_0",
                 "FunSDK/sbpfw/pufrom/chip_{chip}_emu_0",
+		 "feature_sets/boardcfg/{chip}",
                 ]
         sdkpaths.extend(
             [os.path.join(args.sdkdir, path.format(chip=args.chip)) for path in paths_chip_specific])

commit c71d25828f6816587b8004960d2895fbb6fd943f

upgrade: fix downgrade breakage
A typo introduced in 12ba2752f results in invalid choice of skuid eepr
when downgrading and can potentially result in systems failing to boot
due to invalid skuid programmed.

Signed-off-by: Marcin Nowakowski <mnowakowski@microsoft.com>

flash_tools/install_tools/setup.sh
@@ -239,7 +239,7 @@ else
 			./run_fwupgrade.py ${FW_UPGRADE_ARGS} -u eepr --version latest --force --downgrade --select-by-image-type "$host_sku"
 			RC=$?; [ $EXIT_STATUS -eq 0 ] && [ $RC -ne 0 ] && EXIT_STATUS=$RC # only set EXIT_STATUS to error on first error
 
-			./run_fwupgrade.py ${FW_UPGRADE_ARGS} -u eepr --version latest --force --downgrade --active --select-by_image-type "$host_sku"
+			./run_fwupgrade.py ${FW_UPGRADE_ARGS} -u eepr --version latest --force --downgrade --active --select-by-image-type "$host_sku"
 			RC=$?; [ $EXIT_STATUS -eq 0 ] && [ $RC -ne 0 ] && EXIT_STATUS=$RC # only set EXIT_STATUS to error on first error
 
 			log_msg "Downgrading bcfg \"$host_sku\""