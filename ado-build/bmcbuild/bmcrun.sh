86ff8b7e15719871294bd3fe3db62cff435beef2
Charles Gray <charlesgray@microsoft.com>
2024-03-03 18:48:46 -0800

    no, really, full path

--- ado-build/bmcbuild/bmcrun.sh
+++ ado-build/bmcbuild/bmcrun.sh
@@ -3,5 +3,5 @@ sudo docker run --rm --name bmcbuild0 \
     -v ${BUILD_SOURCESDIRECTORY}:/home/bmcbuild/src \
     -v ${BUILD_STAGINGDIRECTORY}:/home/bmcbuild/artifacts \
     bmcbuild \
-    build-i2c-dbg.sh
+    /home/bmcbuild/src/ado-build/bmcbuild/build-i2c-dbg.sh
 
