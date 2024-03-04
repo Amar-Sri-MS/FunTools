6e4e77df73cbf62a979c579158d9f04780c1237d
Charles Gray <charlesgray@microsoft.com>
2024-03-03 19:48:49 -0800

    Clean up artifact production

--- ado-build/bmcbuild/build-i2c-dbg.sh
+++ ado-build/bmcbuild/build-i2c-dbg.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# move to source directory
 cd ~/src/i2c_dbg
-ls /usr/bin
+
+# build all the different platforms
 make MACHINE=fs-bmc-hf
 make MACHINE=rpi
 make MACHINE=qemu
-cp -r build/* ~/artifacts
+
+# copy each build to artifacts drop
+for i in build/* ; do
+    mkdir ~/artifacts/$i
+    cp build/$i/i2c_dbg.so ~/artifacts/$i
+    cp bmc_sbp_chal.py libi2c.so ~/artifacts/$i
+done
