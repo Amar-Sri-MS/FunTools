b60259df7d0d728a92c7cdde59ae7256c89447aa
Charles Gray <charlesgray@microsoft.com>
2024-03-03 19:56:45 -0800

    truncate a shorter path name

--- ado-build/bmcbuild/build-i2c-dbg.sh
+++ ado-build/bmcbuild/build-i2c-dbg.sh
@@ -9,7 +9,8 @@ make MACHINE=rpi
 make MACHINE=qemu
 
 # copy each build to artifacts drop
-for i in build/* ; do
+for p in build/* ; do
+    i=`basename $p`
     mkdir ~/artifacts/$i
     cp build/$i/i2c_dbg.so ~/artifacts/$i
     cp bmc_sbp_chal.py libi2c.so ~/artifacts/$i
